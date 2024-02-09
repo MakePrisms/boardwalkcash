c167cd35a0c07d6c7a98297653a7474886a6367d
austinkelsay <austinkelsay@yahoo.com>
2024-02-09 16:32:17 -0600

    Show pubkey

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Balance = ({ balance }: any) => {
+    const pubkey = window.localStorage.getItem('pubkey');
+
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <h1 className="text-3xl font-bold">{balance} sats</h1>
+            {pubkey ? <h2>pubkey: {pubkey}</h2> : null}
         </div>
     );
 }
