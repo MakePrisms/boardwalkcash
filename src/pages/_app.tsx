f915be897459c9e471f9693cd4e0479cb52c127b
austinkelsay <austinkelsay@yahoo.com>
2024-02-21 13:28:23 -0600

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import {ToastProvider} from "@/hooks/useToast"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ToastProvider>
+        <Analytics />
         <Component {...pageProps} />
       </ToastProvider>
     </Provider>
