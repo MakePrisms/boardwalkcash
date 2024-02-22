6e7838300b95a91c77643f0abea121ce5d868308
daGoodenough <dgoodenough@proton.me>
2024-02-21 16:53:27 -0800

    delete console.logs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,14 +2,10 @@ export const assembleLightningAddress = (pubkey: string, host: string, formatted
   let domain;
 
   if (host.includes('www')) {
-    console.log("INCXLUDES WWW")
     domain = host.split('.')[1] + '.' + host.split('.')[2];
   } else {
-    console.log("NO WWW")
     domain = host
   }
-
-  console.log("DOMAIN", domain)
 
   const user = formatted ? pubkey.slice(0, 5) + '...' + pubkey.slice(-3) : pubkey;
 
