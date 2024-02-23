19f0ee6bdd1ba4fcb475c1b9a37aff726c990d86
daGoodenough <dgoodenough@proton.me>
2024-02-23 14:01:44 -0800

    remove class from disclaimer <p>

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -2,7 +2,7 @@ const Disclaimer = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 text-center p-4 shadow-md flex items-center justify-center">
       <div className="flex items-center justify-center space-x-2">
-        <p className="sm:text-xs md:text-sm font-mono">
+        <p>
           Quick Cashu is still experimental. Use at your own risk!
         </p>
       </div>
