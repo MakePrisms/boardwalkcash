4abecd5eeddd43db8a0949e5c2732ac876abfdf4
daGoodenough <dgoodenough@proton.me>
2024-02-23 11:45:19 -0800

    add ability to pay lightning address or invoice

--- src/components/modals/SendModal.tsx
+++ src/components/modals/SendModal.tsx
@@ -4,6 +4,7 @@ import { getAmountFromInvoice } from "@/utils/bolt11";
 import { useCashu } from "@/hooks/useCashu";
 import { useToast } from "@/hooks/useToast";
 import { CashuWallet } from "@cashu/cashu-ts";
+import { getInvoiceFromLightningAddress } from "@/utils";
 
 interface SendModalProps {
   isSendModalOpen: boolean;
@@ -11,12 +12,22 @@ interface SendModalProps {
   wallet: CashuWallet;
 }
 
+enum Tabs {
+  Destination = "destination",
+  Amount = "amount",
+  Fee = "fee",
+  Send = "send",
+}
+
 export const SendModal = ({
   isSendModalOpen,
   setIsSendModalOpen,
-  wallet
+  wallet,
 }: SendModalProps) => {
-  const [invoice, setInvoice] = useState('');
+  const [currentTab, setCurrentTab] = useState<Tabs>(Tabs.Destination);
+  const [destination, setDestination] = useState("");
+  const [amountSat, setAmountSat] = useState(0);
+  const [invoice, setInvoice] = useState("");
   const [isSending, setIsSending] = useState(false);
   const [estimatedFee, setEstimatedFee] = useState<number | null>(null); // State to store estimated fee
   const [showSubmit, setShowSubmit] = useState(false); // State to control button visibility
@@ -25,18 +36,30 @@ export const SendModal = ({
 
   const { handlePayInvoice } = useCashu();
 
-  const estimateFee = async () => {
-    if (!invoice) {
-      addToast("Please enter an invoice.", "warning");
-      return;
+  const estimateFee = async (toPay?: string) => {
+    if (!toPay) {
+      try {
+        const invoice = await getInvoiceFromLightningAddress(
+          destination,
+          amountSat * 1000
+        );
+        setInvoice(invoice);
+        toPay = invoice;
+      } catch (error) {
+        console.error(error);
+        addToast("An error occurred while fetching the invoice.", "error");
+        return;
+      }
     }
 
     setIsSending(true);
     try {
-      const fee = await wallet.getFee(invoice);
+      console.log("Estimating fee for invoice:", toPay)
+      const fee = await wallet.getFee(toPay!);
       setEstimatedFee(fee);
       setShowSubmit(true); // Show submit button after estimating fee
       addToast(`Estimated fee: ${fee} sats`, "info");
+      setCurrentTab(Tabs.Fee);
     } catch (error) {
       console.error(error);
       addToast("An error occurred while estimating the fee.", "error");
@@ -57,11 +80,102 @@ export const SendModal = ({
       addToast("An error occurred while paying the invoice.", "error");
     }
 
+    setCurrentTab(Tabs.Destination);
+    setDestination("");
     setIsSending(false);
     setInvoice("");
     setEstimatedFee(null);
     setShowSubmit(false);
   };
+
+  const handleDestination = async () => {
+    if (!destination) {
+      addToast("Please enter a destination.", "warning");
+      return;
+    }
+
+    if (destination.startsWith("lnbc")) {
+      console.log("Destination is an invoice");
+      setInvoice(destination);
+      estimateFee(destination);
+      setCurrentTab(Tabs.Fee);
+    } else if (destination.includes("@")) {
+      console.log("Destination is a lightning address");
+      setCurrentTab(Tabs.Amount);
+    }
+  };
+
+  const renderTab = () => {
+    switch (currentTab) {
+      case Tabs.Destination:
+        return (
+          <>
+            <Modal.Body>
+              <input
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                type="text"
+                placeholder="Lightning address or invoice"
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button color="info" onClick={handleDestination}>
+                Continue
+              </Button>
+            </Modal.Footer>
+          </>
+        );
+      case Tabs.Amount:
+        return (
+          <>
+            <Modal.Body>
+              <input
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                type="number"
+                placeholder="Amount in sats"
+                value={amountSat || ""}
+                onChange={(e) =>
+                  setAmountSat(() => parseInt(e.target.value, 10))
+                }
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button color="info" onClick={(e) => estimateFee()}>
+                Continue
+              </Button>
+            </Modal.Footer>
+          </>
+        );
+      case Tabs.Fee:
+        return (
+          <>
+            <Modal.Body>
+              <div className="mt-4 text-sm text-black">
+                Estimated Fee: {estimatedFee} sats
+                <br />
+                Total amount to pay:{" "}
+                {getAmountFromInvoice(invoice) + estimatedFee!} sats
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button color="success" onClick={handleSend}>
+                Pay
+              </Button>
+            </Modal.Footer>
+          </>
+        );
+      case Tabs.Send:
+        return (
+          <div className="flex justify-center items-center my-8">
+            <Spinner size="xl" />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <Modal show={isSendModalOpen} onClose={() => setIsSendModalOpen(false)}>
       <Modal.Header>Send</Modal.Header>
@@ -70,86 +184,8 @@ export const SendModal = ({
           <Spinner size="xl" />
         </div>
       ) : (
-        <>
-          <Modal.Body>
-            <input
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              type="text"
-              placeholder="Lightning address or invoice"
-              value={invoice}
-              onChange={(e) => setInvoice(e.target.value)}
-            />
-            {estimatedFee !== null && (
-              <div className="mt-4 text-sm text-black">
-                Estimated Fee: {estimatedFee} sats
-                <br />
-                Total amount to pay:{" "}
-                {getAmountFromInvoice(invoice) + estimatedFee} sats
-              </div>
-            )}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button color="failure" onClick={() => setIsSendModalOpen(false)}>
-              Cancel
-            </Button>
-            {!showSubmit && (
-              <Button color="info" onClick={estimateFee}>
-                Estimate
-              </Button>
-            )}
-            {showSubmit && (
-              <Button color="success" onClick={handleSend}>
-                Submit
-              </Button>
-            )}
-          </Modal.Footer>
-        </>
+        renderTab()
       )}
     </Modal>
   );
 };
-
-{
-  /* <Modal show={isSendModalOpen} onClose={() => setIsSendModalOpen(false)}>
-                <Modal.Header>Send</Modal.Header>
-                {isSending ? (
-                    <div className="flex justify-center items-center my-8">
-                        <Spinner size="xl" />
-                    </div>
-                ) : (
-                    <>
-                        <Modal.Body>
-                            <input
-                                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                type="text"
-                                placeholder="Lightning address or invoice"
-                                value={invoice}
-                                onChange={(e) => setInvoice(e.target.value)}
-                            />
-                            {estimatedFee !== null && (
-                                <div className="mt-4 text-sm text-black">
-                                    Estimated Fee: {estimatedFee} sats
-                                    <br />
-                                    Total amount to pay: {getAmountFromInvoice(invoice) + estimatedFee} sats
-                                </div>
-                            )}
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button color="failure" onClick={() => setIsSendModalOpen(false)}>
-                                Cancel
-                            </Button>
-                            {!showSubmit && (
-                                <Button color="info" onClick={estimateFee}>
-                                    Estimate
-                                </Button>
-                            )}
-                            {showSubmit && (
-                                <Button color="success" onClick={handleSend}>
-                                    Submit
-                                </Button>
-                            )}
-                        </Modal.Footer>
-                    </>
-                )}
-            </Modal> */
-}
