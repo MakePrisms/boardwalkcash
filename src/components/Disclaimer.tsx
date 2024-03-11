609dde9b2d8b7ebda63e025d335273b1b148bb89
gudnuf <gudnuf21@proton.me>
2024-03-11 13:20:27 -0700

    update disclaimer

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -2,7 +2,7 @@ const Disclaimer = () => {
    return (
       <div>
          <div className='flex items-center justify-center space-x-2'>
-            <p>Quick Cashu is still experimental. Use at your own risk!</p>
+            <p>Quick Cashu is <a className="text-cyan-teal hover:underline" href="https://github.com/MakePrisms/quick-cashu" target="__blank">open source</a> and still experimental. Use at your own risk!</p>
          </div>
       </div>
    );
