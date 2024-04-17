f06e95b3afb9aebff46b40e8a604eadc7032b225
gudnuf <gudnuf21@proton.me>
2024-04-17 13:09:12 -0700

    update lnaddr callback params

--- src/pages/api/lnurlp/[slug].ts
+++ src/pages/api/lnurlp/[slug].ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(200).json({
          callback: `${process.env.NEXT_PUBLIC_PROJECT_URL}/api/callback/${user.pubkey}`,
          maxSendable: 1000000,
-         minSendable: 1000,
+         minSendable: 21000,
          metadata: JSON.stringify(metadata),
          tag: 'payRequest',
       });
