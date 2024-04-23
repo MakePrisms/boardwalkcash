f3aaa9a8f520dbc8c117d96e1489b0f8be5e09aa
gudnuf <gudnuf21@proton.me>
2024-04-23 10:48:32 -0700

    Allow user to set main mint

--- src/components/sidebar/MintSidebarItem.tsx
+++ src/components/sidebar/MintSidebarItem.tsx
@@ -1,7 +1,11 @@
+import { useToast } from '@/hooks/useToast';
+import { setMainKeyset } from '@/redux/slices/Wallet.slice';
+import { RootState, useAppDispatch } from '@/redux/store';
 import { Wallet } from '@/types';
 import { Proof } from '@cashu/cashu-ts';
-import { Badge, Sidebar } from 'flowbite-react';
+import { Badge, Button, Popover, Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 interface MintSidebarItemProps {
    keyset: Wallet;
@@ -9,13 +13,38 @@ interface MintSidebarItemProps {
 
 export const MintSidebarItem = ({ keyset }: MintSidebarItemProps) => {
    const [mintBalance, setMintBalance] = useState('0');
+   const [swapToMainOpen, setSwapToMainOpen] = useState(false);
+   const [setMainOpen, setSetMainOpen] = useState(false);
 
-   const mintHostDomain = keyset.url.replace('https://', '');
+   const { addToast } = useToast();
+   const dispatch = useAppDispatch();
+   const balance = useSelector((state: RootState) => state.wallet.balance);
+
+   const handleSetMain = async () => {
+      console.log('Setting main keyset', keyset.id);
+      await dispatch(setMainKeyset(keyset.id));
+
+      setSetMainOpen(false);
+   };
+
+   const handleSwapToMain = () => {
+      setSwapToMainOpen(false);
+   };
+
+   const handleCopy = () => {
+      try {
+         navigator.clipboard.writeText(keyset.url);
+
+         addToast('Mint URL copied to clipboard', 'success');
+      } catch (e) {
+         addToast('Failed to copy mint URL to clipboard', 'error');
+      }
+   };
 
    useEffect(() => {
       const allProofs = JSON.parse(window.localStorage.getItem('proofs') || '[]') as Proof[];
 
-      const thisProofs = allProofs.filter((proof: any) => proof.keysetId === keyset.id);
+      const thisProofs = allProofs.filter((proof: any) => proof.id === keyset.id);
 
       const thisBalanceCents = thisProofs.reduce(
          (acc: number, proof: any) => acc + proof.amount,
@@ -25,23 +54,98 @@ export const MintSidebarItem = ({ keyset }: MintSidebarItemProps) => {
       const thisBalance = (thisBalanceCents / 100).toFixed(2);
 
       setMintBalance(thisBalance);
-   }, [keyset]);
+   }, [keyset, balance]);
+
+   const formattedMintUrl = () => {
+      const mintHostDomain = keyset.url.replace('https://', '');
+
+      return `${mintHostDomain.slice(0, 15)}...${mintHostDomain.slice(-5)}`;
+   };
 
    return (
       <Sidebar.Item className='w-full'>
-         <div className='flex justify-between min-w-full'>
-            <div>{`${mintHostDomain.slice(0, 10)}...${mintHostDomain.slice(-10)}`}</div>
-            &nbsp;
-            <div>
+         <div className='flex flex-col justify-between min-w-full'>
+            <div className='flex justify-between'>
+               {formattedMintUrl()} <Badge>${mintBalance}</Badge>
+            </div>
+            <div className='flex justify-between align-middle min-w-max'>
+               <Popover
+                  open={setMainOpen}
+                  content={
+                     <div className='w-80 text-sm text-gray-400'>
+                        <div className='border-b border-gray-200 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-gray-700'>
+                           <h3
+                              id='set-main-popover'
+                              className='font-semibold text-gray-900 dark:text-white'
+                           >
+                              Set Main
+                           </h3>
+                        </div>
+                        <div className='px-3 py-2 mb-3'>
+                           <p className='whitespace-break-spaces break-words'>
+                              {`Set ${keyset.url} as main sending and receiving mint?`}
+                           </p>
+                        </div>
+                        <div className='flex justify-around mb-3 mr-3'>
+                           <Button onClick={() => setSetMainOpen(false)} color='failure'>
+                              Cancel
+                           </Button>
+                           <Button onClick={handleSetMain} color='success'>
+                              Confirm
+                           </Button>
+                        </div>
+                     </div>
+                  }
+               >
+                  <button onClick={() => setSetMainOpen(true)} className='text-xs underline mr-3'>
+                     Set Main
+                  </button>
+               </Popover>
+               <Popover
+                  open={swapToMainOpen}
+                  content={
+                     <div className='w-80 text-sm text-gray-400'>
+                        <div className='border-b border-gray-200 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-gray-700'>
+                           <h3
+                              id='swap-popover'
+                              className='font-semibold text-gray-900 dark:text-white'
+                           >
+                              Swap to Main
+                           </h3>
+                        </div>
+                        <div className='px-3 py-2'>
+                           <p className='whitespace-break-spaces break-words'>
+                              Swap all the funds from {keyset.url} to the main mint?
+                           </p>
+                        </div>
+                        <div className='flex justify-around mb-3 mr-3'>
+                           <Button onClick={() => setSwapToMainOpen(false)} color='failure'>
+                              Cancel
+                           </Button>
+                           <Button onClick={handleSwapToMain} color='success'>
+                              Confirm
+                           </Button>
+                        </div>
+                     </div>
+                  }
+               >
+                  <button
+                     onClick={() => setSwapToMainOpen(true)}
+                     className='text-xs mr-3 underline'
+                  >
+                     Swap to Main
+                  </button>
+               </Popover>
+               <button onClick={handleCopy} className='text-xs underline mr-3'>
+                  Copy
+               </button>
                {keyset.active ? (
-                  <Badge color='success'>Default</Badge>
+                  <Badge className='' color='success'>
+                     Main
+                  </Badge>
                ) : (
                   <div className='text-transparent'>Inacdve</div>
-                  // <Badge color='failure'>Inactive</Badge>
                )}
-            </div>
-            <div>
-               <Badge>${mintBalance}</Badge>
             </div>
          </div>
       </Sidebar.Item>
--- src/redux/slices/Wallet.slice.ts
+++ src/redux/slices/Wallet.slice.ts
@@ -1,7 +1,7 @@
-import { MintKeys, MintKeyset, Proof } from '@cashu/cashu-ts';
+import { MintKeys, Proof } from '@cashu/cashu-ts';
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Wallet } from '@/types';
-import { act } from 'react-dom/test-utils';
+import { RootState } from '../store';
 
 const updateKeysetLocalStorage = (keysets: { [key: string]: Wallet }) => {
    localStorage.setItem('keysets', JSON.stringify(Object.values(keysets)));
@@ -19,18 +19,72 @@ const initialState: WalletState = {
    keysets: {},
 };
 
-export const initializeKeysets = createAsyncThunk<{ keysets: Wallet[] }>(
+export const initializeKeysets = createAsyncThunk<{ keysets: Wallet[]; balance: { usd: number } }>(
    'wallet/initializeKeysets',
    async (_, { rejectWithValue }) => {
       const keysets = JSON.parse(localStorage.getItem('keysets') || '[]') as Wallet[];
-      return { keysets };
+      const proofs = JSON.parse(localStorage.getItem('proofs') || '[]') as Proof[];
+      const balance = proofs.reduce(
+         (acc, proof) => {
+            acc.usd += proof.amount;
+            return acc;
+         },
+         { usd: 0 },
+      );
+      return { keysets, balance };
    },
 );
+
+export const setMainKeyset = createAsyncThunk(
+   'wallet/setMainKeyset',
+   async (keysetId: string, { getState, dispatch }) => {
+      const state = getState() as RootState;
+
+      const toSetMain = state.wallet.keysets[keysetId];
+
+      if (!toSetMain) {
+         throw new Error('Keyset not found');
+      }
+
+      if (toSetMain.active) {
+         console.log('This keyset is already active.');
+         return;
+      }
+
+      try {
+         const pubkey = localStorage.getItem('pubkey');
+         console.log('Posting to /api/users/', pubkey);
+         await fetch(`/api/users/${pubkey}`, {
+            method: 'PUT',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ mintUrl: toSetMain.url, pubkey }),
+         });
+      } catch (e) {
+         throw new Error(`Failed to update user: ${e}`);
+      }
+
+      return { keysetId, active: true };
+   },
+);
+
+interface UpdateKeysetActiveStatusPayload {
+   id: string;
+   active: boolean;
+}
 
 export const walletSlice = createSlice({
    name: 'walletSlice',
    initialState,
    reducers: {
+      updateKeysetActiveStatus: (state, action: PayloadAction<UpdateKeysetActiveStatusPayload>) => {
+         const { id, active } = action.payload;
+         const keyset = state.keysets[id];
+         if (keyset) {
+            keyset.active = active;
+         }
+      },
       setBalance: (state, action: PayloadAction<{ [unit: string]: number }>) => {
          if (state.balanceLocked) {
             return;
@@ -48,7 +102,6 @@ export const walletSlice = createSlice({
          action: PayloadAction<{ keyset: MintKeys; url: string; active?: boolean }>,
       ) => {
          const { keyset, url } = action.payload;
-         // const active = Object.keys(state.keysets).length > 0 ? false : true;
          const toAdd: Wallet = {
             id: keyset.id,
             keys: keyset,
@@ -62,21 +115,38 @@ export const walletSlice = createSlice({
       },
    },
    extraReducers: builder => {
-      builder.addCase(
-         initializeKeysets.fulfilled,
-         (state, action: PayloadAction<{ keysets: Wallet[] }>) => {
-            state.keysets = action.payload.keysets.reduce(
-               (acc, keyset) => {
-                  acc[keyset.id] = keyset;
-                  return acc;
-               },
-               {} as { [key: string]: Wallet },
-            );
-         },
-      );
+      builder
+         .addCase(
+            initializeKeysets.fulfilled,
+            (state, action: PayloadAction<{ keysets: Wallet[]; balance: { usd: number } }>) => {
+               state.keysets = action.payload.keysets.reduce(
+                  (acc, keyset) => {
+                     acc[keyset.id] = keyset;
+                     return acc;
+                  },
+                  {} as { [key: string]: Wallet },
+               );
+               state.balance = action.payload.balance;
+            },
+         )
+         .addCase(setMainKeyset.fulfilled, (state, action) => {
+            if (!action.payload) return;
+            const { keysetId, active = true } = action.payload;
+            const keyset = state.keysets[keysetId];
+            if (keyset) {
+               keyset.active = active;
+               Object.values(state.keysets).forEach(k => {
+                  if (k.id !== keysetId) {
+                     k.active = false;
+                  }
+               });
+               updateKeysetLocalStorage(state.keysets);
+            }
+         });
    },
 });
 
-export const { setBalance, lockBalance, unlockBalance, addKeyset } = walletSlice.actions;
+export const { setBalance, lockBalance, unlockBalance, addKeyset, updateKeysetActiveStatus } =
+   walletSlice.actions;
 
 export default walletSlice.reducer;
