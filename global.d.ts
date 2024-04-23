c19e422ead67e4730478befc4a0fd6f69b9fabbc
gudnuf <gudnuf21@proton.me>
2024-04-23 11:29:24 -0700

    make prisma global

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,7 @@
+import { PrismaClient } from '@prisma/client';
+
 declare module 'light-bolt11-decoder';
+
+declare global {
+   var prisma: PrismaClient;
+}
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (!global.prisma) {
+   global.prisma = new PrismaClient();
+}
+prisma = global.prisma;
 
 export default prisma;
