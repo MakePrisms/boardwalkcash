f14e419c68564d6c1d7745e4ac9cf435d343ccb8
gudnuf <gudnuf21@proton.me>
2024-04-29 12:13:20 -0700

    Add animated QR to send ecash

---
+++ src/components/modals/SendEcashModalBody.tsx
@@ -0,0 +1,114 @@
+import { useCashu } from '@/hooks/useCashu';
+import { useToast } from '@/hooks/useToast';
+import {
+   CashuWallet,
+   Proof,
+   Token,
+   TokenEntry,
+   getDecodedToken,
+   getEncodedToken,
+} from '@cashu/cashu-ts';
+import { Modal } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import AnimatedQRCode from '../AnimatedQR';
+import ClipboardButton from '../buttons/utility/ClipboardButton';
+
+interface SendEcashModalBodyProps {
+   amountUsd: number;
+}
+
+const SendEcashModalBody = ({ amountUsd }: SendEcashModalBodyProps) => {
+   const [sending, setSending] = useState(false);
+   const [sendAmount, setSendAmount] = useState('');
+   const [tokenEntryData, setTokenEntryData] = useState<{
+      proofs: Proof[];
+      wallet: CashuWallet;
+   } | null>(null);
+   const [encodedToken, setEncodedToken] = useState<string | null>(null);
+   const [urFragment, setURFragment] = useState<string | null>(null);
+
+   const { addToast } = useToast();
+   const { swapToSend } = useCashu();
+
+   useEffect(() => {
+      if (!tokenEntryData) return;
+
+      const token: Token = {
+         token: [
+            {
+               proofs: tokenEntryData.proofs,
+               mint: tokenEntryData.wallet.mint.mintUrl,
+            } as TokenEntry,
+         ],
+         unit: 'usd',
+      };
+
+      const encodedToken = getEncodedToken(token);
+
+      console.log('Encoded Token', encodedToken);
+
+      console.log('DECODED', getDecodedToken(encodedToken));
+
+      setEncodedToken(encodedToken.replace('Token:', ''));
+   }, [tokenEntryData]);
+
+   useEffect(() => {
+      const handleSendEcash = async () => {
+         console.log('Send Ecash', amountUsd);
+         if (amountUsd <= 0) {
+            addToast('Enter an amount to send', 'error');
+            return;
+         }
+         setSending(true);
+
+         console.log(
+            'TOtal balance. About to send',
+            (JSON.parse(window.localStorage.getItem('proofs') || '[]') as Proof[]).reduce(
+               (a, b) => a + b.amount,
+               0,
+            ),
+         );
+
+         const { proofs: newProofs, wallet } = await swapToSend(amountUsd);
+
+         console.log(
+            'Balance after sending',
+            (JSON.parse(window.localStorage.getItem('proofs') || '[]') as Proof[]).reduce(
+               (a, b) => a + b.amount,
+               0,
+            ),
+         );
+
+         console.log('Send Ecash', newProofs);
+
+         if (!newProofs) {
+            return;
+         }
+
+         setTokenEntryData({ proofs: newProofs, wallet });
+      };
+
+      handleSendEcash();
+   }, [amountUsd]);
+   return (
+      <>
+         <Modal.Body>
+            {!sending ? (
+               <div>Loading...</div>
+            ) : (
+               <div className='flex flex-col justify-center items-center my-8 text-black space-y-3'>
+                  {encodedToken && (
+                     <>
+                        <AnimatedQRCode encodedToken={encodedToken} />
+                        <p> Token: {`${encodedToken.slice(0, 12)}...${encodedToken.slice(-5)}`}</p>
+                        <ClipboardButton toCopy={encodedToken} toShow={`Copy`} />
+                     </>
+                  )}
+               </div>
+            )}
+         </Modal.Body>
+      </>
+   );
+};
+
+export default SendEcashModalBody;
--- src/components/modals/SendModal.tsx
+++ src/components/modals/SendModal.tsx
@@ -8,6 +8,7 @@ import { RootState } from '@/redux/store';
 import { useSelector } from 'react-redux';
 import { useExchangeRate } from '@/hooks/useExchangeRate';
 import SendEcashButton from '../buttons/ecash/SendEcashButton';
+import SendEcashModalBody from './SendEcashModalBody';
 
 interface SendModalProps {
    isSendModalOpen: boolean;
@@ -19,6 +20,7 @@ enum Tabs {
    Amount = 'amount',
    Fee = 'fee',
    Send = 'send',
+   Ecash = 'ecash',
 }
 
 export const SendModal = ({ isSendModalOpen, setIsSendModalOpen }: SendModalProps) => {
@@ -152,6 +154,10 @@ export const SendModal = ({ isSendModalOpen, setIsSendModalOpen }: SendModalProp
          setCurrentTab(Tabs.Fee);
       } else if (destination.includes('@')) {
          setCurrentTab(Tabs.Amount);
+      } else if (!isNaN(parseFloat(destination))) {
+         console.log('AMount entered as destination');
+         setAmountSat(destination);
+         setCurrentTab(Tabs.Ecash);
       }
    };
 
@@ -169,12 +175,11 @@ export const SendModal = ({ isSendModalOpen, setIsSendModalOpen }: SendModalProp
                      <input
                         className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mb-4'
                         type='text'
-                        placeholder='Lightning address or invoice'
+                        placeholder='Amount USD, Lightning address, or invoice'
                         value={destination}
                         onChange={e => setDestination(e.target.value)}
                      />
                      <div className='flex justify-end'>
-                        <SendEcashButton onClick={handleSendEcash} />
                         <Button color='info' onClick={handleDestination}>
                            Continue
                         </Button>
@@ -234,13 +239,16 @@ export const SendModal = ({ isSendModalOpen, setIsSendModalOpen }: SendModalProp
                </div>
             );
 
+         case Tabs.Ecash:
+            return <SendEcashModalBody amountUsd={parseFloat(destination) * 100} />;
+
          default:
             return null;
       }
    };
 
    return (
-      <Modal show={isSendModalOpen} onClose={() => setIsSendModalOpen(false)}>
+      <Modal show={isSendModalOpen} onClose={resetModalState}>
          <Modal.Header>Send</Modal.Header>
          {isProcessing ? (
             <div className='flex justify-center items-center my-8'>
