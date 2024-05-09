6849917460507fba0306d19be23cf62524f0d814
gudnuf <gudnuf21@proton.me>
2024-05-09 12:27:45 -0700

    fix nwcState issues

--- src/hooks/useExchangeRate.ts
+++ src/hooks/useExchangeRate.ts
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useExchangeRate = () => {
    const [lastFetched, setLastFetched] = useState<number>(0);
    const [usdToSatRate, setUsdToSatRate] = useState<number>(0);
 
-   const fetchUsdToSatRate = async () => {
+   const fetchUsdToSatRate = useCallback(async () => {
       // const storedRate = localStorage.getItem('usdToSatRate');
 
       if (lastFetched > Date.now() - 1000 * 60 * 5) {
@@ -27,31 +27,37 @@ export const useExchangeRate = () => {
       const usdSat = usdBtc / 100_000_000;
       setUsdToSatRate(usdSat);
       return usdSat;
-   };
+   }, [lastFetched, usdToSatRate]);
 
-   const unitToSats = async (amount: number, unit: string): Promise<number> => {
-      switch (unit) {
-         case 'sat':
-            return amount;
-         case 'usd':
-            const exchangeRate = await fetchUsdToSatRate();
-            return Math.round(amount / exchangeRate);
-         default:
-            throw new Error('Invalid unit');
-      }
-   };
+   const unitToSats = useCallback(
+      async (amount: number, unit: string): Promise<number> => {
+         switch (unit) {
+            case 'sat':
+               return amount;
+            case 'usd':
+               const exchangeRate = await fetchUsdToSatRate();
+               return Math.floor(amount / exchangeRate);
+            default:
+               throw new Error('Invalid unit');
+         }
+      },
+      [fetchUsdToSatRate],
+   );
 
-   const satsToUnit = async (amount: number, unit: string): Promise<number> => {
-      switch (unit) {
-         case 'sat':
-            return amount;
-         case 'usd':
-            const exchangeRate = await fetchUsdToSatRate();
-            return Math.round(amount * exchangeRate * 100);
-         default:
-            throw new Error('Invalid unit');
-      }
-   };
+   const satsToUnit = useCallback(
+      async (amount: number, unit: string): Promise<number> => {
+         switch (unit) {
+            case 'sat':
+               return amount;
+            case 'usd':
+               const exchangeRate = await fetchUsdToSatRate();
+               return Math.floor(amount * exchangeRate * 100);
+            default:
+               throw new Error('Invalid unit');
+         }
+      },
+      [fetchUsdToSatRate],
+   );
 
    return { unitToSats, satsToUnit };
 };
