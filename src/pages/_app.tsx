2b070fccabff6534595d25118e22ca39422fdfd7
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-05-15 21:14:23 -0700

    added pwa configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <>
          <Head>
             <title>Boardwalk Cash</title>
+            <link rel='manifest' href='/manifest.json' />
+            <meta name='theme-color' content='#0a1f44' />
+            <link rel='icon' href='/favicon.ico' />
+            <meta
+               name='viewport'
+               content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+            />
+            <meta
+               name='description'
+               content='A Lightning / Cashu Ecash wallet designed for fast easy onboarding and use'
+            />
          </Head>
          <Provider store={store}>
             <PersistGate persistor={persistor} loading={null}>
