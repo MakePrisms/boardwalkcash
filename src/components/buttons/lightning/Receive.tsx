147a23f6969af48af1105461ac47119a08e0b104
gudnuf <gudnuf21@proton.me>
2024-05-17 18:09:38 -0700

    add scanner to receive modal

--- src/components/buttons/lightning/Receive.tsx
+++ src/components/buttons/lightning/Receive.tsx
@@ -12,6 +12,7 @@ import QRCode from 'qrcode.react';
 import { RootState } from '@/redux/store';
 import ConfirmEcashReceiveModal from '@/components/modals/ConfirmEcashReceiveModal';
 import { Token } from '@cashu/cashu-ts';
+import QRScannerButton from '../QRScannerButton';
 
 const Receive = () => {
    const [isReceiveModalOpen, setIsReceiveModalOpen] = useState(false);
@@ -27,6 +28,23 @@ const Receive = () => {
    const dispatch = useDispatch();
    const wallets = useSelector((state: RootState) => state.wallet.keysets);
 
+   const getTokenFromUrl = (url: string) => {
+      try {
+         const urlObj = new URL(url);
+         const params = new URLSearchParams(urlObj.search);
+         const token = params.get('token');
+
+         if (token && token.startsWith('cashu')) {
+            return token;
+         }
+
+         return null;
+      } catch (error) {
+         console.error('Invalid URL:', error);
+         return null;
+      }
+   };
+
    useEffect(() => {
       // timeout for the pubKey to be set in localStorage on first load
       setTimeout(() => {
@@ -40,7 +58,15 @@ const Receive = () => {
    }, []);
 
    useEffect(() => {
-      const decoded = decodeToken(amount);
+      let decoded: Token | undefined = undefined;
+      if (amount.includes('http')) {
+         const encoded = getTokenFromUrl(amount);
+         if (encoded) {
+            decoded = decodeToken(encoded);
+         }
+      } else {
+         decoded = decodeToken(amount);
+      }
 
       if (!decoded) return;
 
@@ -164,6 +190,7 @@ const Receive = () => {
                               >
                                  &nbsp;&nbsp;Generate Invoice&nbsp;
                               </Button>
+                              <QRScannerButton onScan={setAmount} />
                               <Tooltip content='Copy lightning address'>
                                  <ClipboardButton
                                     onClick={handleModalClose}
