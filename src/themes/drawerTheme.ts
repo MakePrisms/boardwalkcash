64921c26c82f6db5d17ad370c7131560552f66a6
gudnuf <gudnuf21@proton.me>
2024-05-26 15:22:12 -0700

    mod drawer theme

--- src/themes/drawerTheme.ts
+++ src/themes/drawerTheme.ts
@@ -15,7 +15,7 @@ export const customDrawerTheme: DeepPartial<FlowbiteDrawerTheme> = {
          titleText:
             'mb-4 text-xl inline-flex items-center text-base font-semibold text-gray-200 dark:text-gray-400',
          closeButton:
-            'absolute  end-2.5 top-2.5 flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white',
+            'absolute  end-2.5 top-2.5 flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-white',
 
          closeIcon: 'h-4 w-4',
       },
