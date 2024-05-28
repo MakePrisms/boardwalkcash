9392532ed30b06075f840e05fcc1f542a9186f50
Damian <108303703+gudnuf@users.noreply.github.com>
2024-05-27 17:37:20 -0700

    Catch QR error when token is too long (#52)
    * update warning message
    
    * catch QR error when token is too long

--- src/components/modals/SendEcashModalBody.tsx
+++ src/components/modals/SendEcashModalBody.tsx
@@ -4,6 +4,7 @@ import AnimatedQRCode from '../AnimatedQR';
 import ClipboardButton from '../buttons/utility/ClipboardButton';
 import QRCode from 'qrcode.react';
 import CustomCarousel from '../Carousel/CustomCarousel';
+import ErrorBoundary from '../ErrorBoundary';
 
 interface SendEcashModalBodyProps {
    token: string;
@@ -12,24 +13,44 @@ interface SendEcashModalBodyProps {
 
 const SendEcashModalBody = ({ onClose, token }: SendEcashModalBodyProps) => {
    const [carouselSlides, setCarouselSlides] = useState<React.ReactNode[]>([]);
+   const [qrError, setQrError] = useState(false);
 
    useEffect(() => {
       if (!token) return;
-      setCarouselSlides([
-         <div className='text-black text-center space-y-2 ml-3' key='1'>
-            <h2 className='text-xl'>Shareable Boardwalk Cash Link</h2>
-            <QRCode
-               value={`${window.location.protocol}//${window.location.host}/wallet?token=${token}`}
-               size={window.innerWidth < 768 ? 275 : 400}
-            />
-            <p> Link: {`boardwalkcash.com...`}</p>
-         </div>,
-         <div className='text-black text-center space-y-2' key='2'>
-            <h2 className='text-xl'>Ecash Token</h2>
-            <AnimatedQRCode encodedToken={`${token}`} />
-            <p> Token: {`${token.slice(0, 12)}...${token.slice(-5)}`}</p>
-         </div>,
-      ]);
+
+      try {
+         const qrCodeValue = `${window.location.protocol}//${window.location.host}/wallet?token=${token}`;
+
+         setCarouselSlides([
+            <div className='text-black text-center space-y-2 ml-3' key='1'>
+               <ErrorBoundary
+                  fallback={
+                     <p className='text-red-500'>
+                        Failed to load QR. Link too long. Go to transaction history to reclaim your
+                        tokens and try again, or just copy the link by clicking the button below.{' '}
+                     </p>
+                  }
+               >
+                  <h2 className='text-xl'>Shareable Boardwalk Cash Link</h2>
+                  <QRCode value={qrCodeValue} size={window.innerWidth < 768 ? 275 : 400} />
+                  <p> Link: {`boardwalkcash.com...`}</p>
+               </ErrorBoundary>
+            </div>,
+            <div className='text-black text-center space-y-2' key='2'>
+               <h2 className='text-xl'>Ecash Token</h2>
+               <ErrorBoundary
+                  fallback={<p className='text-red-500'>Failed to load QR. Token too long</p>}
+               >
+                  <AnimatedQRCode encodedToken={`${token}`} />
+               </ErrorBoundary>
+               <p> Token: {`${token.slice(0, 12)}...${token.slice(-5)}`}</p>
+            </div>,
+         ]);
+      } catch (error) {
+         console.error(error);
+         setQrError(true);
+      }
+
       return () => {};
    }, [token]);
 
