10229e9a5106877407305097f9da57f5cebfd18b
Damian <108303703+gudnuf@users.noreply.github.com>
2024-06-21 15:27:00 -0700

    only allow one tab to be open (#64)

---
+++ src/pages/already-running.tsx
@@ -0,0 +1,19 @@
+import { Button } from 'flowbite-react';
+
+const AlreadyRunning = () => {
+   return (
+      <main
+         className='flex flex-col items-center justify-center mx-auto md:max-w-screen-sm space-y-4'
+         style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+      >
+         <h1 className='text-3xl'>Already Running</h1>
+         <p className='text-center'>
+            It looks like you already have BoardwalkCash running in another tab. Close one and
+            retry.
+         </p>
+         <Button onClick={() => (window.location.href = '/wallet')}>Retry</Button>
+      </main>
+   );
+};
+
+export default AlreadyRunning;
