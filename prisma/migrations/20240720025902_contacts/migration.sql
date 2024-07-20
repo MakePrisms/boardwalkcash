0ff43cb2b2f9bb168ab5486482cf0aeb9765c12a
gudnuf <gudnuf21@proton.me>
2024-07-19 20:00:59 -0700

    add contacts and view them (#77)

---
+++ prisma/migrations/20240720025902_contacts/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "nickname" TEXT,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "xHandle" TEXT,
+    "userId" TEXT NOT NULL,
+    "linkedUserId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("pubkey") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_linkedUserId_fkey" FOREIGN KEY ("linkedUserId") REFERENCES "User"("pubkey") ON DELETE SET NULL ON UPDATE CASCADE;
