d54973c768f42830748c7419de92c9b8ba736058
gudnuf <gudnuf21@proton.me>
2024-07-23 09:46:21 -0700

    fix some bugs in adding contacts

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,21 +1,23 @@
 import ClipboardButton from '@/components/buttons/utility/ClipboardButton';
 import { useToast } from '@/hooks/util/useToast';
-import { RootState } from '@/redux/store';
+import { ContactData } from '@/lib/userModels';
 import { PublicContact } from '@/types';
 import { Button } from 'flowbite-react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import QRCode from 'qrcode.react';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
 
 const UserProfilePage = () => {
    const [boardwalkInitialized, setBoardwalkInitialized] = React.useState(false);
    const [user, setUser] = React.useState<PublicContact | null>(null);
+   const [localPubkey, setLocalPubkey] = React.useState<string | null>(null);
    const [isLoaded, setIsLoaded] = React.useState(false);
+   const [showAddContact, setShowAddContact] = React.useState(false);
+   const [addingContact, setAddingContact] = React.useState(false);
+
    const router = useRouter();
    const { addToast } = useToast();
-   const { user: activeUser } = useSelector((state: RootState) => state);
 
    React.useEffect(() => {
       const username = router.query.slug as string;
@@ -32,7 +34,7 @@ const UserProfilePage = () => {
          } else {
             if (res.status === 404) {
                console.error('User not found', username);
-               // router.push('/404/user');
+               router.push('/404/user');
             }
          }
       };
@@ -42,41 +44,67 @@ const UserProfilePage = () => {
 
          if (localPubkey) {
             setBoardwalkInitialized(true);
+            setShowAddContact(true);
+            setLocalPubkey(localPubkey);
          }
       };
 
       const loadState = async () => {
          await fetchUser();
          isBoardwalkInitialized();
+
          setIsLoaded(true);
       };
 
       loadState();
    }, [router.query.slug]);
 
-   if (!isLoaded || !user) {
-      return <div>Loading...</div>;
-   }
-
-   const handleAddContact = async () => {
+   const handleAddContact = useCallback(async () => {
       if (!boardwalkInitialized) {
          addToast('Setup Boardwalk at boarwalkcash.com', 'error');
       }
 
-      const res = await fetch(`/api/users/${activeUser.pubkey}`, {
+      if (!user) {
+         return;
+      }
+
+      if (user.pubkey === localPubkey) {
+         addToast('You cannot add yourself as a contact', 'error');
+         setShowAddContact(false);
+         return;
+      }
+
+      setAddingContact(true);
+
+      const res = await fetch(`/api/users/${localPubkey}`, {
          method: 'PUT',
          headers: {
             'Content-Type': 'application/json',
          },
-         body: JSON.stringify({ contact: user.pubkey }),
+         body: JSON.stringify({
+            contact: user.pubkey,
+            linkedUserPubkey: user.pubkey,
+         } as ContactData),
       });
 
       if (!res.ok) {
-         addToast('Failed to add contact', 'error');
+         if (res.status === 409) {
+            addToast('Contact already added', 'error');
+            setShowAddContact(false);
+         } else {
+            addToast('Failed to add contact', 'error');
+         }
       } else {
          addToast('Contact added', 'success');
+         setShowAddContact(false);
       }
-   };
+
+      setAddingContact(false);
+   }, [localPubkey, user, boardwalkInitialized, addingContact]);
+
+   if (!isLoaded || !user) {
+      return <div>Loading...</div>;
+   }
 
    return (
       <>
@@ -90,14 +118,18 @@ const UserProfilePage = () => {
             <meta property='og:description' content={`${user.username}'s Boardwalk`} />
          </Head>
          <main
-            className='flex flex-col items-center justify-center mx-auto space-y-5'
+            className='flex flex-col items-center justify-center mx-auto space-y-7'
             style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
          >
             <h1 className='text-3xl'>{user.username}</h1>
             <QRCode value={`${window.location.href}`} size={128} />
-            <div className='flex'>
+            <div className='flex flex-row items-center space-x-6'>
                <ClipboardButton toCopy={user.username!} toShow='Username' />
-               {boardwalkInitialized && <Button onClick={handleAddContact}>Add Contact</Button>}
+               {showAddContact && (
+                  <Button isProcessing={addingContact} onClick={handleAddContact}>
+                     Add Contact
+                  </Button>
+               )}
             </div>
          </main>
       </>
