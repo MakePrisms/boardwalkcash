ed43bceb60eaaa0327e15265042c5c003cc4b7a4
gudnuf <gudnuf21@proton.me>
2024-09-06 09:54:36 -0700

    add min width to leaderboard drawer

--- src/components/leaderboards/LeaderboardDrawer.tsx
+++ src/components/leaderboards/LeaderboardDrawer.tsx
@@ -16,7 +16,7 @@ const LeaderboardDrawer = ({ isOpen, onClose }: LeaderboardDrawerProps) => {
             onClose={onClose}
             edge={false}
             position='right'
-            className='h-full bg-[#0f1f41ff] text-white flex flex-col md:min-w-fit min-w-full'
+            className='h-full bg-[#0f1f41ff] text-white flex flex-col md:min-w-[400px] min-w-full'
             theme={customDrawerTheme}
          >
             <Drawer.Header
