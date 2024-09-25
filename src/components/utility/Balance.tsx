50504505257ca3e21f650d258245bcd7c2b15bf9
gudnuf <gudnuf21@proton.me>
2024-09-25 16:41:05 -0700

    add balance toggle

--- src/components/utility/Balance.tsx
+++ src/components/utility/Balance.tsx
@@ -1,10 +1,15 @@
 import { useCashuContext } from '@/hooks/contexts/cashuContext';
 import React, { useState, useEffect } from 'react';
+import { useExchangeRate } from '@/hooks/util/useExchangeRate';
+import { formatSats } from '@/utils/formatting';
+import { format } from 'path';
 
 const Balance = ({ balanceByWallet }: { balanceByWallet: Record<string, number> }) => {
-   const { wallets, activeWallet, activeUnit: unit } = useCashuContext();
+   const { wallets, activeWallet, activeUnit } = useCashuContext();
    const [usdBalance, setUsdBalance] = useState(0);
    const [satBalance, setSatBalance] = useState(0);
+   const [showFxValue, setShowFxValue] = useState(false);
+   const { satsToUnit, unitToSats } = useExchangeRate();
 
    useEffect(() => {
       if (!wallets.size) return;
@@ -28,26 +33,50 @@ const Balance = ({ balanceByWallet }: { balanceByWallet: Record<string, number> 
    }, [balanceByWallet, wallets]);
 
    const handleClick = () => {
-      // setUnit(prevUnit => (prevUnit === 'sat' ? 'usd' : 'sat'));
+      setShowFxValue(prev => !prev);
    };
 
    const formatUsdBalance = (balance: number) => {
       return (balance / 100).toFixed(2);
    };
 
-   const unitSymbol = unit === 'usd' ? '$' : '₿';
+   const getDisplayBalance = async () => {
+      if (!showFxValue) {
+         return activeUnit === 'usd' ? formatUsdBalance(usdBalance) : satBalance.toString();
+      } else {
+         if (activeUnit === 'usd') {
+            const sats = await unitToSats(usdBalance, 'usd');
+            return sats.toLocaleString();
+         } else {
+            const usd = await satsToUnit(satBalance, 'usd');
+            return formatUsdBalance(usd);
+         }
+      }
+   };
+
+   const [displayBalance, setDisplayBalance] = useState('');
+
+   useEffect(() => {
+      getDisplayBalance().then(setDisplayBalance);
+   }, [showFxValue, usdBalance, satBalance, activeUnit]);
+
+   const unitSymbol = showFxValue
+      ? activeUnit === 'usd'
+         ? '₿'
+         : '$'
+      : activeUnit === 'usd'
+        ? '$'
+        : '₿';
 
    return (
       <div className='flex flex-col items-center justify-center w-full mb-14'>
-         <div className='' onClick={handleClick}>
-            {unit === 'usd' && (
-               <span className=' text-5xl text-cyan-teal font-bold'>{unitSymbol}</span>
+         <div className='cursor-pointer' onClick={handleClick}>
+            {activeUnit === 'usd' && !showFxValue && (
+               <span className='text-5xl text-cyan-teal font-bold'>{unitSymbol}</span>
             )}
-            <span className='font-teko text-6xl font-bold'>
-               {unit === 'usd' ? formatUsdBalance(usdBalance) : satBalance}
-            </span>{' '}
-            {unit === 'sat' && (
-               <span className=' text-5xl text-cyan-teal font-bold'>{unitSymbol}</span>
+            <span className='font-teko text-6xl font-bold'>{displayBalance}</span>{' '}
+            {(activeUnit === 'sat' || showFxValue) && (
+               <span className='text-5xl text-cyan-teal font-bold'>{unitSymbol}</span>
             )}
          </div>
       </div>
