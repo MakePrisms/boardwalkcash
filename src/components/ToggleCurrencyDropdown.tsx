1d1d31f6f661ab9ee3adf9d4e7978cd6256cba82
gudnuf <gudnuf21@proton.me>
2024-09-25 16:47:42 -0700

    change currency toggle to just a button

--- src/components/ToggleCurrencyDropdown.tsx
+++ src/components/ToggleCurrencyDropdown.tsx
@@ -2,20 +2,39 @@ import React from 'react';
 import { Dropdown } from 'flowbite-react';
 import { useCashuContext } from '@/hooks/contexts/cashuContext';
 import { Currency } from '@/types';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid';
 
 const ToggleCurrencyDropdown = () => {
    const { activeUnit, setActiveUnit, defaultWallets } = useCashuContext();
 
    const availableUnits = Array.from(defaultWallets.keys());
 
+   const handleClick = () => {
+      const otherUnit = availableUnits.find(unit => unit !== activeUnit);
+      console.log('otherUnit', otherUnit);
+      if (otherUnit) {
+         setActiveUnit(otherUnit as Currency);
+      }
+   };
+
    return (
-      <Dropdown label={activeUnit.toUpperCase()} inline>
-         {availableUnits.map(unit => (
-            <Dropdown.Item key={unit} onClick={() => setActiveUnit(unit as Currency)}>
-               {unit.toUpperCase()}
-            </Dropdown.Item>
-         ))}
-      </Dropdown>
+      <button onClick={handleClick}>
+         <div className='flex items-center space-x-2'>
+            {activeUnit.toUpperCase()}
+            {activeUnit === Currency.USD ? (
+               <ChevronDownIcon className='h-5 w-5' />
+            ) : (
+               <ChevronUpIcon className='h-5 w-5' />
+            )}
+         </div>
+      </button>
+      // <Dropdown onClick={handleClick} label={activeUnit.toUpperCase()} inline>
+      //    {/* {availableUnits.map(unit => (
+      //       <Dropdown.Item key={unit} onClick={() => setActiveUnit(unit as Currency)}>
+      //          {unit.toUpperCase()}
+      //       </Dropdown.Item>
+      //    ))} */}
+      // </Dropdown>
    );
 };
 
