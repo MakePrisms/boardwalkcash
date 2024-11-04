237cc6b2d8101c1068f68ea0bbc083cf33631390
gudnuf <gudnuf21@proton.me>
2024-11-04 11:16:40 -0800

    handle amountless payment request

--- src/hooks/util/useNumpad.tsx
+++ src/hooks/util/useNumpad.tsx
@@ -58,8 +58,17 @@ export const useNumpad = ({ showDecimal }: UseNumpadProps = {}) => {
 
    const numpadValueIsEmpty = inputValue === '' || inputValue === '.';
 
+   const numpadAmount = useMemo(() => {
+      const parsedAmount = parseFloat(inputValue);
+      if (isNaN(parsedAmount)) {
+         return;
+      }
+      return activeUnit === Currency.USD ? parsedAmount * 100 : parsedAmount;
+   }, [inputValue]);
+
    return {
       numpadValue: inputValue,
+      numpadAmount,
       numpadValueIsEmpty,
       setInputValue,
       handleNumpadInput,
