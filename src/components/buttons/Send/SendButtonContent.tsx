ada1125117a4fd8280fe52868032612e44e91c08
gudnuf <gudnuf21@proton.me>
2024-11-04 04:45:22 -0800

    move some send logic to useWallet

--- src/components/buttons/Send/SendButtonContent.tsx
+++ src/components/buttons/Send/SendButtonContent.tsx
@@ -2,7 +2,6 @@ import { decodePaymentRequest, MeltQuoteResponse, PaymentRequest } from '@cashu/
 import PaymentConfirmationDetails from '@/components/views/PaymentConfirmationDetails';
 import ConfrimAndSendLightning from '@/components/views/ConfirmAndSendLightning';
 import { usePaymentRequests } from '@/hooks/cashu/usePaymentRequests';
-import useNotifications from '@/hooks/boardwalk/useNotifications';
 import ConfirmSendGift from '@/components/views/ConfirmSendGift';
 import useMintlessMode from '@/hooks/boardwalk/useMintlessMode';
 import { useCashuContext } from '@/hooks/contexts/cashuContext';
@@ -13,7 +12,6 @@ import { Currency, GiftAsset, PublicContact } from '@/types';
 import SelectContact from '@/components/views/SelectContact';
 import Amount from '@/components/utility/amounts/Amount';
 import ShareEcash from '@/components/views/ShareEcash';
-import { postTokenToDb } from '@/utils/appApiRequests';
 import SelectGift from '@/components/views/SelectGift';
 import { getAmountFromInvoice } from '@/utils/bolt11';
 import QRScanner from '@/components/views/QRScanner';
@@ -26,7 +24,7 @@ import { useToast } from '@/hooks/util/useToast';
 import { Tabs } from '@/components/utility/Tabs';
 import PasteButton from '../utility/PasteButton';
 import Numpad from '@/components/utility/Numpad';
-import { formatUnit } from '@/utils/formatting';
+import useWallet from '@/hooks/boardwalk/useWallet';
 import { useState } from 'react';
 
 const SendButtonContent = ({
@@ -53,8 +51,8 @@ const SendButtonContent = ({
       txid?: string;
       gift?: GiftAsset;
    } | null>(null);
-   const [meltQuote, setMeltQuote] = useState<MeltQuoteResponse | undefined>(undefined);
    const [paymentRequest, setPaymentRequest] = useState<PaymentRequest | undefined>(undefined);
+   const [meltQuote, setMeltQuote] = useState<MeltQuoteResponse | undefined>(undefined);
    const [contact, setContact] = useState<PublicContact | undefined>(undefined);
    const [amtUnit, setAmtUnit] = useState<number | undefined>(undefined);
    const [invoice, setInvoice] = useState<string | undefined>(undefined);
@@ -62,12 +60,12 @@ const SendButtonContent = ({
    const [lud16, setLud16] = useState<string | undefined>(undefined);
    const [isGiftMode, setIsGiftMode] = useState(false);
 
-   const { isMintless, createMintlessToken, sendToMintlessUser } = useMintlessMode();
-   const { createSendableToken, getMeltQuote } = useCashu();
    const { unitToSats, convertToUnit } = useExchangeRate();
-   const { sendTokenAsNotification } = useNotifications();
    const { payPaymentRequest } = usePaymentRequests();
+   const { sendGift, sendEcash } = useWallet();
+   const { isMintless } = useMintlessMode();
    const { activeUnit } = useCashuContext();
+   const { getMeltQuote } = useCashu();
    const { addToast } = useToast();
    const {
       handleNumpadBackspace,
@@ -102,9 +100,33 @@ const SendButtonContent = ({
       const amtUnit = activeUnit === Currency.USD ? parsedAmount * 100 : parsedAmount;
       setAmtUnit(amtUnit);
       if (activeInputTab === 'ecash') {
-         return handleSendEcash(amtUnit, activeUnit, gift);
+         return handleSendEcash(amtUnit);
       } else if (activeInputTab === 'lightning') {
          setCurrentView('lud16Input');
+      }
+   };
+
+   const handleSendEcash = async (amount: number) => {
+      if (!amount) {
+         addToast('Please enter an amount', 'error');
+         return;
+      }
+      const res = await sendEcash(amount, activeUnit, resetState, gift, contact);
+      if (res) {
+         setShareTokenData({ token: res.token, txid: res.txid, gift });
+         setCurrentView('shareEcash');
+      }
+   };
+
+   const handleSendGift = async () => {
+      if (!gift) {
+         addToast('Please select a gift', 'error');
+         return;
+      }
+      const res = await sendGift(gift, resetState, contact);
+      if (res) {
+         setShareTokenData({ token: res.token, txid: res.txid, gift });
+         setCurrentView('shareEcash');
       }
    };
 
@@ -154,58 +176,6 @@ const SendButtonContent = ({
       }
    };
 
-   const handleSendEcash = async (amount: number, unit: Currency, gift?: GiftAsset) => {
-      setCurrentView;
-      let token: string | undefined;
-      try {
-         if (isMintless && !contact?.mintlessReceive) {
-            /* if we are making a lightning payment to a user with a mint */
-            if (!contact) {
-               addToast('You can only send eTips and eGifts from a Lightning Wallet.', 'error');
-               throw new Error('You can only send eTips and eGifts from a Lightning Wallet.');
-            }
-            if (unit !== 'sat') {
-               throw new Error('mintless only supports sat');
-            }
-
-            token = await createMintlessToken(Math.round(amount), unit, contact);
-         } else if (contact?.mintlessReceive) {
-            /* user wants to receive to their lud16 */
-            if (!contact.lud16) {
-               addToast('Contact does not have a lightning address', 'error');
-               return;
-            }
-
-            const transaction = await sendToMintlessUser(amount, activeUnit, contact);
-            resetState();
-         } else {
-            /* regular send */
-            console.log('creating token for ', amount);
-            token = await createSendableToken(amount, {
-               pubkey: contact ? `02${contact.pubkey}` : undefined,
-               gift: gift?.name,
-               feeCents: gift?.fee,
-            });
-         }
-
-         if (!token) {
-            throw new Error('Failed to create ecash token');
-         }
-
-         let txid: string | undefined;
-         if (contact) {
-            await sendTokenAsNotification(token);
-            txid = await postTokenToDb(token);
-         }
-
-         setShareTokenData({ token, txid, gift });
-         setCurrentView('shareEcash');
-      } catch (e: any) {
-         console.error(e);
-         // resetState();
-      }
-   };
-
    const handlePaste = async (pastedValue: string) => {
       const cleanedValue = pastedValue.toLowerCase().replace('lightning:', '');
       if (cleanedValue.startsWith('lnbc')) {
@@ -236,77 +206,6 @@ const SendButtonContent = ({
       } else {
          addToast('Invalid input', 'error');
          return;
-      }
-   };
-
-   const handleSendGift = async () => {
-      if (!gift) {
-         addToast('Please select a gift', 'error');
-         return;
-      }
-      try {
-         if (gift?.campaingId) {
-            throw new Error('Gift campaigns not implemented');
-            // if (!selectedContact) throw new Error('No contact selected');
-            // const { token } = await sendCampaignGift(gift, selectedContact?.pubkey).catch(e => {
-            //    const errMsg = e.message || 'Failed to send eGift';
-            //    addToast(errMsg, 'error');
-            //    setSending(false);
-            //    return { token: null };
-            // });
-            // if (token) {
-            //    addToast(`eGift sent to ${selectedContact?.username}`, 'success');
-            //    setToken(token);
-            //    setCurrentStep(GiftStep.ShareGift);
-            // }
-            // setSending(false);
-            // return;
-         }
-         // else if (useInvoice) {
-         //    handleLightningTip(amountUnit, gift?.fee);
-         //    return;
-         // }
-         let sendableToken: string | undefined;
-         if (isMintless && !contact?.mintlessReceive) {
-            if (!contact) {
-               throw new Error('No contact selected');
-            }
-
-            sendableToken = await createMintlessToken(gift.amount, activeUnit, contact, gift?.name);
-         } else if (contact?.mintlessReceive) {
-            await sendToMintlessUser(gift.amount, activeUnit, contact, gift?.name);
-
-            addToast(`eGift sent`, 'success');
-            resetState();
-         } else {
-            sendableToken = await createSendableToken(gift.amount, {
-               pubkey: contact ? `02${contact.pubkey}` : undefined,
-               gift: gift?.name,
-               feeCents: gift?.fee,
-            });
-         }
-
-         if (!sendableToken) {
-            /* this error case is handled in useCashu */
-            return;
-         }
-
-         let txid: string | undefined;
-         if (contact) {
-            txid = await postTokenToDb(sendableToken, gift?.name);
-            await sendTokenAsNotification(sendableToken, txid);
-         }
-
-         addToast(
-            `eGift sent (${formatUnit(gift.amount + (gift?.fee || 0), activeUnit)})`,
-            'success',
-         );
-         setShareTokenData({ token: sendableToken, txid, gift });
-         setCurrentView('shareEcash');
-      } catch (error: any) {
-         console.error('Error sending token:', error);
-         const msg = error.message || 'Failed to send token';
-         addToast(msg, 'error');
       }
    };
 
---
+++ src/hooks/boardwalk/useWallet.tsx
@@ -0,0 +1,172 @@
+import { Currency, GiftAsset, PublicContact } from '@/types';
+import { useCashuContext } from '../contexts/cashuContext';
+import { postTokenToDb } from '@/utils/appApiRequests';
+import useNotifications from './useNotifications';
+import { formatUnit } from '@/utils/formatting';
+import useMintlessMode from './useMintlessMode';
+import { useCashu } from '../cashu/useCashu';
+import { useToast } from '../util/useToast';
+
+const useWallet = () => {
+   const { isMintless, createMintlessToken, sendToMintlessUser } = useMintlessMode();
+   const { sendTokenAsNotification } = useNotifications();
+   const { createSendableToken } = useCashu();
+   const { activeUnit } = useCashuContext();
+   const { addToast } = useToast();
+
+   /**
+    * Send ecash or make a mintless transaction
+    * @param amount Amount of the unit to send
+    * @param unit Unit to send
+    * @param onSendToMintlessUser Callback that should reset the parent component's state because sending to a mintless user just returns void
+    * @param gift Optional gift to send with the ecash
+    * @param contact Contact to lock the ecash too and send a notification to
+    * @returns If succesfful and not sending to a mintless user it returns sent data, otherwise undefined (including on error)
+    */
+   const sendEcash = async (
+      amount: number,
+      unit: Currency,
+      onSendToMintlessUser: () => void,
+      gift?: GiftAsset,
+      contact?: PublicContact,
+   ): Promise<
+      | {
+           token: string;
+           txid: string | undefined;
+           gift: GiftAsset | undefined;
+        }
+      | undefined
+   > => {
+      let token: string | undefined;
+      try {
+         if (isMintless && !contact?.mintlessReceive) {
+            /* if we are making a lightning payment to a user with a mint */
+            if (!contact) {
+               addToast('You can only send eTips and eGifts from a Lightning Wallet.', 'error');
+               throw new Error('You can only send eTips and eGifts from a Lightning Wallet.');
+            }
+            if (unit !== 'sat') {
+               throw new Error('mintless only supports sat');
+            }
+
+            token = await createMintlessToken(Math.round(amount), unit, contact);
+         } else if (contact?.mintlessReceive) {
+            /* user wants to receive to their lud16 */
+            if (!contact.lud16) {
+               addToast('Contact does not have a lightning address', 'error');
+               return;
+            }
+
+            await sendToMintlessUser(amount, activeUnit, contact);
+            onSendToMintlessUser();
+         } else {
+            /* regular send */
+            console.log('creating token for ', amount);
+            token = await createSendableToken(amount, {
+               pubkey: contact ? `02${contact.pubkey}` : undefined,
+               gift: gift?.name,
+               feeCents: gift?.fee,
+            });
+         }
+
+         if (!token) {
+            throw new Error('Failed to create ecash token');
+         }
+
+         let txid: string | undefined;
+         if (contact) {
+            await sendTokenAsNotification(token);
+            txid = await postTokenToDb(token);
+         }
+
+         return { token, txid, gift };
+      } catch (e: any) {
+         console.error(e);
+      }
+   };
+
+   /**
+    * Send a gift as ecash or to a mintless user
+    * @param gift [`GiftAsset`] to send
+    * @param onSendToMintlessUser Called when sending to a mintless user. Should reset the parent component's state
+    * @param contact Optional contact to send the gift to
+    * @returns If succesfful and not sending to a mintless user it returns sent data, otherwise undefined (including on error)
+    */
+   const sendGift = async (
+      gift: GiftAsset,
+      onSendToMintlessUser: () => void,
+      contact?: PublicContact,
+   ): Promise<{ txid?: string; token?: string } | undefined> => {
+      try {
+         if (gift?.campaingId) {
+            throw new Error('Gift campaigns not implemented');
+            // if (!selectedContact) throw new Error('No contact selected');
+            // const { token } = await sendCampaignGift(gift, selectedContact?.pubkey).catch(e => {
+            //    const errMsg = e.message || 'Failed to send eGift';
+            //    addToast(errMsg, 'error');
+            //    setSending(false);
+            //    return { token: null };
+            // });
+            // if (token) {
+            //    addToast(`eGift sent to ${selectedContact?.username}`, 'success');
+            //    setToken(token);
+            //    setCurrentStep(GiftStep.ShareGift);
+            // }
+            // setSending(false);
+            // return;
+         }
+         // else if (useInvoice) {
+         //    handleLightningTip(amountUnit, gift?.fee);
+         //    return;
+         // }
+         let sendableToken: string | undefined;
+         if (isMintless && !contact?.mintlessReceive) {
+            if (!contact) {
+               throw new Error('No contact selected');
+            }
+
+            sendableToken = await createMintlessToken(gift.amount, activeUnit, contact, gift?.name);
+         } else if (contact?.mintlessReceive) {
+            await sendToMintlessUser(gift.amount, activeUnit, contact, gift?.name);
+
+            addToast(`eGift sent`, 'success');
+            onSendToMintlessUser();
+            return undefined;
+         } else {
+            sendableToken = await createSendableToken(gift.amount, {
+               pubkey: contact ? `02${contact.pubkey}` : undefined,
+               gift: gift?.name,
+               feeCents: gift?.fee,
+            });
+         }
+
+         if (!sendableToken) {
+            /* this error case is handled in useCashu */
+            return;
+         }
+
+         let txid: string | undefined;
+         if (contact) {
+            txid = await postTokenToDb(sendableToken, gift?.name);
+            await sendTokenAsNotification(sendableToken, txid);
+         }
+
+         addToast(
+            `eGift sent (${formatUnit(gift.amount + (gift?.fee || 0), activeUnit)})`,
+            'success',
+         );
+         return { txid, token: sendableToken };
+         //  setCurrentView('shareEcash');
+      } catch (error: any) {
+         console.error('Error sending token:', error);
+         const msg = error.message || 'Failed to send token';
+         addToast(msg, 'error');
+      }
+   };
+   return {
+      sendGift,
+      sendEcash,
+   };
+};
+
+export default useWallet;
