0d3d44fee5da422a2b68329b5d0bebb5a2ba3794
gudnuf <gudnuf21@proton.me>
2024-11-20 13:38:30 -0800

    fix bug when calculating invoice expiry

--- src/utils/bolt11.ts
+++ src/utils/bolt11.ts
@@ -18,8 +18,16 @@ export const decodeBolt11 = (invoice: string) => {
    const amountSection = findSection(decoded.sections, 'amount');
    const amountSat = amountSection?.value ? Number(amountSection.value) / 1000 : undefined;
 
-   const expiryUnixSeconds = decoded.expiry;
-   const expiryUnixMs = decoded.expiry * 1000;
+   const expirySection = findSection(decoded.sections, 'expiry');
+   const timestampSection = findSection(decoded.sections, 'timestamp');
 
-   return { amountSat, expiryUnixMs };
+   /* infinity if no expiry is defined */
+   let expiryUnixSec = Infinity;
+   if (expirySection && timestampSection) {
+      expiryUnixSec = timestampSection.value + expirySection.value;
+   } else if (decoded.expiry && timestampSection) {
+      expiryUnixSec = timestampSection.value + decoded.expiry;
+   }
+
+   return { amountSat, expiryUnixMs: expiryUnixSec * 1000 };
 };
