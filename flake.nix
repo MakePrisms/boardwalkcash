70ea12d7a31f89f4b5798bb75d55b82996d3f293
gudnuf <gudnuf21@proton.me>
2024-12-05 00:40:21 -0800

    remove bad configs

--- flake.nix
+++ flake.nix
@@ -202,10 +202,8 @@ EOF
               mkdir -p .lightning/l1
 
               bitcoind -datadir="$BITCOIN_DIR" -regtest -txindex -fallbackfee=0.00000253 -daemon
-              lightningd --network=regtest --lightning-dir=$LIGHTNING_DIR --daemon --log-level=debug --log-file=$LIGHTNING_DIR/l1/log --addr=localhost:7171 --allow-deprecated-apis=false --developer --dev-fast-gossip --dev-bitcoind-poll=5 --experimental-dual-fund --experimental-splicing --experimental-offers --funder-policy=match --funder-policy-mod=100 --funder-min-their-funding=10000 --funder-per-channel-max=100000 --funder-fuzz-percent=0 --funder-lease-requests-only --lease-fee-base-sat=2sat --lease-fee-basis=50 --invoices-onchain-fallback
+              lightningd --network=regtest --lightning-dir=$LIGHTNING_DIR --daemon --log-level=debug --log-file=$LIGHTNING_DIR/l1/log --addr=localhost:7171 --allow-deprecated-apis=false 
               cdk-mintd --work-dir=./.cashu/mint1 &
-              # stop_ln
-              # destroy_ln
             '';
           };
         };
