5df2d543015a455228376296de3cfbdb6404ddef
gudnuf <gudnuf21@proton.me>
2024-12-12 18:28:42 -0800

    use zustand for animated-navigation

--- app/lib/animated-navigation.tsx
+++ app/lib/animated-navigation.tsx
@@ -1,76 +1,81 @@
-// creates wrappers around useNavigate and Link from @remix-run/react
-// to add animated navigation
+// requires that view-transitions and animations are setup in in the app's tailwind.css
 
 import {
   Link as RemixLink,
   useNavigate as useRemixNavigate,
 } from '@remix-run/react';
-import { createContext, useContext, useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { create } from 'zustand';
 
 type AnimationDirection = 'left' | 'right' | null;
 
-interface AnimatedNavigationContextType {
-  setDirection: (direction: AnimationDirection) => void;
+interface AnimatedNavigationStore {
   direction: AnimationDirection;
+  setDirection: (direction: AnimationDirection) => void;
 }
 
-const AnimatedNavigationContext =
-  createContext<AnimatedNavigationContextType | null>(null);
+const useStore = create<AnimatedNavigationStore>((set) => ({
+  direction: null,
+  setDirection: (direction) => set({ direction }),
+}));
 
+const DIRECTION_ANIMATIONS: Record<
+  NonNullable<AnimationDirection>,
+  {
+    out: string;
+    in: string;
+  }
+> = {
+  right: {
+    out: 'slideOutToRight',
+    in: 'slideInFromLeft',
+  },
+  left: {
+    out: 'slideOutToLeft',
+    in: 'slideInFromRight',
+  },
+} as const;
+
+/** wrap the app with this to apply the styles based on direction*/
 export function AnimatedNavigationProvider({
   children,
 }: { children: React.ReactNode }) {
-  const [direction, setDirection] = useState<AnimationDirection>(null);
+  const direction = useStore((state) => state.direction);
 
   useEffect(() => {
-    // these animations are defined in app/tailwind.css
-    if (direction === 'right') {
+    if (direction) {
+      const animations = DIRECTION_ANIMATIONS[direction];
       document.documentElement.style.setProperty(
         '--direction-out',
-        'slideOutToRight',
+        animations.out,
       );
       document.documentElement.style.setProperty(
         '--direction-in',
-        'slideInFromLeft',
-      );
-    } else if (direction === 'left') {
-      document.documentElement.style.setProperty(
-        '--direction-out',
-        'slideOutToLeft',
-      );
-      document.documentElement.style.setProperty(
-        '--direction-in',
-        'slideInFromRight',
+        animations.in,
       );
     }
   }, [direction]);
 
-  return (
-    <AnimatedNavigationContext.Provider value={{ direction, setDirection }}>
-      {children}
-    </AnimatedNavigationContext.Provider>
-  );
-}
-
-export function useAnimatedNavigation() {
-  const context = useContext(AnimatedNavigationContext);
-  if (!context) {
-    throw new Error(
-      'useAnimatedNavigation must be used within AnimatedNavigationProvider',
-    );
-  }
-  return context;
+  return children;
 }
 
 interface AnimatedLinkProps extends React.ComponentProps<typeof RemixLink> {
   direction?: 'left' | 'right';
 }
 
+/**
+ * A wrapper around Link that when used will animate the page transitions.
+ *
+ * Default is to prefetch the link when it is rendered to optimize the mobile experience,
+ * but this can be overridden by setting the prefetch prop.
+ *
+ * @param props - direction: 'left' | 'right'
+ */
 export function AnimatedLink({
   direction = 'left',
   ...props
 }: AnimatedLinkProps) {
-  const { setDirection } = useAnimatedNavigation();
+  const setDirection = useStore((state) => state.setDirection);
 
   return (
     <RemixLink
@@ -85,9 +90,10 @@ export function AnimatedLink({
   );
 }
 
+/** a wrapper around useNavigate that will animate the page transitions */
 export function useAnimatedNavigate() {
   const navigate = useRemixNavigate();
-  const { setDirection } = useAnimatedNavigation();
+  const setDirection = useStore((state) => state.setDirection);
 
   return (to: string, options?: { direction?: 'left' | 'right' }) => {
     setDirection(options?.direction ?? 'left');
