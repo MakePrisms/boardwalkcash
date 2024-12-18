c3a90b0165e96e79bc1d27bb22fab3517c678792
Josip Bojčić <jbojcic1@gmail.com>
2024-12-18 20:03:46 +0100

    Fetch rates with react query (#225)

--- app/routes/_protected._index.tsx
+++ app/routes/_protected._index.tsx
@@ -1,4 +1,5 @@
-import { NavLink, useOutletContext } from '@remix-run/react';
+import { NavLink } from '@remix-run/react';
+import { useQuery } from '@tanstack/react-query';
 import { Cog } from 'lucide-react';
 import { type SubmitHandler, useForm } from 'react-hook-form';
 import { Page, PageHeader } from '~/components/page';
@@ -13,7 +14,11 @@ import { useTheme } from '~/features/theme';
 import { useAuthActions } from '~/features/user/auth';
 import { useUserStore } from '~/features/user/user-provider';
 import { toast } from '~/hooks/use-toast';
-import { type ExchangeRates, convertToUnit } from '~/lib/exchange-rate';
+import {
+  type ExchangeRates,
+  convertToUnit,
+  fetchRates,
+} from '~/lib/exchange-rate';
 import { formatUnit } from '~/lib/formatting';
 import { LinkWithViewTransition } from '~/lib/transitions';
 import { buildEmailValidator } from '~/lib/validation';
@@ -57,7 +62,19 @@ export default function Index() {
   );
   const { theme, effectiveColorMode, colorMode, setTheme, setColorMode } =
     useTheme();
-  const { rates } = useOutletContext<{ rates: ExchangeRates }>();
+  const { data: rates } = useQuery({
+    queryKey: ['exchangeRate'],
+    queryFn: () => {
+      console.log('fetching rates from the client');
+      return fetchRates('average');
+    },
+    // This is a workaround to make the type of the data not have | undefined.
+    // In our case the initial data will be what was prefetched on the server but react query doesn't know that we are
+    // doing prefetching there. I asked a question here to see if there is a better way
+    // https://github.com/TanStack/query/discussions/1331#discussioncomment-11607342
+    initialData: {} as ExchangeRates,
+  });
+  console.log('rates', rates);
 
   const {
     register,
