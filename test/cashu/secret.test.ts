e91f97dbe16605a7c7bd88f72c0f5750ac191cb4
gudnuf <gudnuf21@proton.me>
2024-12-22 19:37:08 -0800

    update tesdescription

--- test/cashu/secret.test.ts
+++ test/cashu/secret.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'bun:test';
 import { getP2PKPubkeyFromSecret, parseSecret } from '~/lib/cashu/secret';
 
 describe('parseSecret', () => {
-  test('should return a secret as described in NUT00', () => {
+  test('should return the input value if input value is NUT00 plain secret', () => {
     const s = parseSecret(
       '859d4935c4907062a6297cf4e663e2835d90d97ecdd510745d32f6816323a41f',
     );
