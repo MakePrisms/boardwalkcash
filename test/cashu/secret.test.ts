105c899fbf7005034f79cd75e7b2f8ccbb1056ae
gudnuf <gudnuf21@proton.me>
2024-12-22 19:33:48 -0800

    add more tests

--- test/cashu/secret.test.ts
+++ test/cashu/secret.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'bun:test';
-import { parseSecret } from '~/lib/cashu/secret';
+import { getP2PKPubkeyFromSecret, parseSecret } from '~/lib/cashu/secret';
 
 describe('parseSecret', () => {
   test('should return a secret as described in NUT00', () => {
@@ -11,9 +11,92 @@ describe('parseSecret', () => {
     );
   });
 
-  test('should throw if secret is not in WELL_KNOWN_SECRET_KINDS', () => {
-    expect(() =>
-      parseSecret('["HTLC",{"nonce":"0","data":"0","tags":[]}]'),
-    ).toThrow();
+  test('should successfully parse a valid NUT10 secret', () => {
+    const s = parseSecret(
+      '["P2PK",{"nonce":"abc123","data":"0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7","tags":[["sigflag","SIG_INPUTS"]]}]',
+    );
+    expect(s).toEqual({
+      kind: 'P2PK',
+      nonce: 'abc123',
+      data: '0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7',
+      tags: [['sigflag', 'SIG_INPUTS']],
+    });
+  });
+
+  describe('should not throw if', () => {
+    test('tags are not provided', () => {
+      const s = parseSecret(
+        '["P2PK",{"nonce":"abc123","data":"0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7"}]',
+      );
+      expect(s).toEqual({
+        kind: 'P2PK',
+        nonce: 'abc123',
+        data: '0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7',
+      });
+    });
+
+    test('a tag has multiple values', () => {
+      const s = parseSecret(
+        '["P2PK",{"nonce":"abc123","data":"0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7","tags":[["sigflag","a","b"]]}]',
+      );
+      expect(s).toEqual({
+        kind: 'P2PK',
+        nonce: 'abc123',
+        data: '0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7',
+        tags: [['sigflag', 'a', 'b']],
+      });
+    });
+  });
+  describe('should throw for invalid NUT10 secret if', () => {
+    test('secret is not in WELL_KNOWN_SECRET_KINDS', () => {
+      expect(() =>
+        parseSecret('["HTLC",{"nonce":"0","data":"0","tags":[]}]'),
+      ).toThrow();
+    });
+
+    test('nonce is not a string', () => {
+      expect(() => parseSecret('["P2PK",{"nonce":123,"data":"test"}]')).toThrow(
+        'Invalid secret format',
+      );
+    });
+
+    test('data is not a string', () => {
+      expect(() =>
+        parseSecret('["P2PK",{"nonce":"test","data":false}]'),
+      ).toThrow('Invalid secret format');
+    });
+
+    test('tags is not an array of string arrays', () => {
+      expect(() =>
+        parseSecret(
+          '["P2PK",{"nonce":"test","data":"test","tags":["invalid"]}]',
+        ),
+      ).toThrow('Invalid secret format');
+    });
+  });
+});
+
+describe('getP2PKPubkeyFromSecret', () => {
+  test('should return the public key from a P2PK secret', () => {
+    const pubkey = getP2PKPubkeyFromSecret(
+      '["P2PK",{"nonce":"abc123","data":"0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7"}]',
+    );
+    expect(pubkey).toBe(
+      '0249098aa8b9d2fbec49ff8598feb17b592b986e62319a4fa488a3dc36387157a7',
+    );
+  });
+
+  describe('should throw if', () => {
+    test('secret is not a P2PK secret', () => {
+      expect(() => getP2PKPubkeyFromSecret('not-a-p2pk-secret')).toThrow(
+        'Secret is not a P2PK secret',
+      );
+    });
+
+    test('secret is an invalid NUT10 secret', () => {
+      expect(() =>
+        getP2PKPubkeyFromSecret('["P2PK",{"nonce":123,"data":"test"}]'),
+      ).toThrow('Invalid secret format');
+    });
   });
 });
