5af20d3722ace285156d6bcc32b5d6c4eaaba60d
gudnuf <gudnuf21@proton.me>
2024-12-26 10:15:20 -0800

    add tests and strip 'US'

--- app/lib/money/money.ts
+++ app/lib/money/money.ts
@@ -36,7 +36,12 @@ const currencyDataMap: CurrencyDataMap = {
         symbol: '$',
         factor: new Big(1),
         format: function (value: number, options: FormatOptions = {}) {
-          return baseFormat(value, { ...options, decimals: this.decimals });
+          let formattedValue = baseFormat(value, {
+            ...options,
+            decimals: this.decimals,
+          });
+          formattedValue = formattedValue.replace('US', '').trim();
+          return formattedValue;
         },
       },
       {
@@ -49,7 +54,10 @@ const currencyDataMap: CurrencyDataMap = {
             ...options,
             decimals: this.decimals,
           });
-          formattedValue = formattedValue.replace('$', '').trim();
+          formattedValue = formattedValue
+            .replace('$', '')
+            .replace('US', '')
+            .trim();
           return `${formattedValue}${this.symbol}`;
         },
       },
