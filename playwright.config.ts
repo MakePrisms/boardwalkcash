60ae342524b464f17717510edb74754269e54dcb
Josip Bojčić <jbojcic1@gmail.com>
2024-12-28 21:19:11 +0100

    Change playwright config to run the app in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: process.env.CI ? 'bun run build && bun run start' : 'bun run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
