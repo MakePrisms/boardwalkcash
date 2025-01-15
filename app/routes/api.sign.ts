70e6f57c5c13e1a62ffb4276055f0d6a1d692f32
Josip Bojčić <jbojcic1@gmail.com>
2025-01-15 22:39:03 +0100

    Setup supabase

---
+++ app/routes/api.sign.ts
@@ -0,0 +1,25 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import sign from 'jwt-encode';
+
+// TODO: this endpoint should check for presence of OS jwt and valdate that sub in the OS jwt matches the sub in the request body
+export async function action({ request }: ActionFunctionArgs) {
+  if (request.method !== 'POST') {
+    return new Response('Method not allowed', { status: 405 });
+  }
+  const body = await request.json();
+
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    console.error('JWT_SECRET is not set');
+    return new Response('Error. Failed to sign the payload', { status: 500 });
+  }
+
+  const jwt = sign(body, secret);
+  const responseBody = JSON.stringify({ jwt });
+
+  return new Response(responseBody, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
