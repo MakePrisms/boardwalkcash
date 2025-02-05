7707ccb4ccb8dfd222a05f4b8063f33dc0fe077b
gudnuf <108303703+gudnuf@users.noreply.github.com>
2025-02-04 20:15:20 -0800

    useNumberInput - change initialization arguments (#296)
    * change initialization arguments
    
    * Money input cleanup (#297)
    
    ---------
    
    Co-authored-by: Josip Bojčić <jbojcic1@gmail.com>

--- app/hooks/use-exchange-rate.ts
+++ app/hooks/use-exchange-rate.ts
@@ -1,66 +1,30 @@
 import { useQuery } from '@tanstack/react-query';
 import { type Ticker, exchangeRateService } from '~/lib/exchange-rate';
 
-type ExchangeRateResult =
-  | {
-      rate: string;
-      isLoading: false;
-      error: null;
-    }
-  | {
-      rate: undefined;
-      isLoading: true;
-      error: null;
-    }
-  | {
-      rate: undefined;
-      isLoading: false;
-      error: Error;
-    };
+export const useExchangeRate = (ticker: Ticker) => {
+  return useQuery({
+    queryKey: ['exchangeRate', ticker],
+    queryFn: async ({ signal }) => {
+      return exchangeRateService
+        .getRates({
+          tickers: [ticker],
+          signal,
+        })
+        .then((rates) => rates[ticker]);
+    },
+    refetchInterval: 15_000,
+  });
+};
 
-export const useExchangeRate = (ticker: Ticker): ExchangeRateResult => {
-  const {
-    data: rates,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['exchangeRate'],
+export const useExchangeRates = (tickers: Ticker[]) => {
+  return useQuery({
+    queryKey: ['exchangeRate', tickers],
     queryFn: async ({ signal }) => {
       return exchangeRateService.getRates({
-        tickers: ['BTC-USD', 'USD-BTC', 'USD-USD', 'BTC-BTC'],
+        tickers,
         signal,
       });
     },
     refetchInterval: 15_000,
   });
-
-  if (isLoading) {
-    return {
-      rate: undefined,
-      isLoading: true,
-      error: null,
-    };
-  }
-
-  if (error) {
-    return {
-      rate: undefined,
-      isLoading: false,
-      error: error instanceof Error ? error : new Error('Unknown error'),
-    };
-  }
-
-  if (!rates) {
-    return {
-      rate: undefined,
-      isLoading: false,
-      error: new Error(`Rate not found for ticker ${ticker}`),
-    };
-  }
-
-  return {
-    rate: rates[ticker],
-    isLoading: false,
-    error: null,
-  };
 };
