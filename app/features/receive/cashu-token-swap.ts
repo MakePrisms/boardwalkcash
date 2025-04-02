2164773d61048d967e4094f3483bfc5909ec5ee8
gudnuf <gudnuf21@proton.me>
2025-04-02 11:59:01 -0700

    swap to claim cashu tokens

---
+++ app/features/receive/cashu-token-swap.ts
@@ -0,0 +1,41 @@
+import type { Proof } from '@cashu/cashu-ts';
+import type { Money } from '~/lib/money';
+
+/**
+ * A token swap is the process of receiving a Cashu token into
+ * the user's account that matches the mint of the token by using
+ * the `/v1/swap` endpoint of the mint as defined in [NUT-03](https://github.com/cashubtc/nuts/blob/main/03.md).
+ *
+ * A swap is created in the database when a user inputs a token and chooses
+ * the matching receive account.
+ *
+ * All PENDING swaps are tracked upon insert and completed in the background.
+ */
+export type CashuTokenSwap = {
+  /** Hash of the token being received used to identify the swap */
+  tokenHash: string;
+  /** Proofs from the token being received */
+  tokenProofs: Proof[];
+  /** ID of the user receiving the token */
+  userId: string;
+  /** ID of the account receiving the token */
+  accountId: string;
+  /** Amount of the token being received in the corresponding currency */
+  amount: Money;
+  /** ID of the keyset used for blinded messages */
+  keysetId: string;
+  /** Starting counter value used to generate the blinded messages */
+  keysetCounter: number;
+  /** Amounts for each blinded message */
+  outputAmounts: number[];
+  /**
+   * Current state of the token swap
+   *
+   * - PENDING: the swap was created, but we still need to swap with the mint and store the proofs
+   * - COMPLETED: the swap is completed, and the proofs have been stored
+   * - FAILED: the swap failed
+   */
+  state: 'PENDING' | 'COMPLETED' | 'FAILED';
+  /** Timestamp when the token swap was created */
+  createdAt: string;
+};
---
+++ app/features/receive/receive-cashu-token-hooks.tsx
@@ -0,0 +1,322 @@
+import type { Proof, Token } from '@cashu/cashu-ts';
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import type { Account, CashuAccount } from '~/features/accounts/account';
+import {
+  useAccounts,
+  useDefaultAccount,
+} from '~/features/accounts/account-hooks';
+import { tokenToMoney } from '~/features/shared/cashu';
+import {
+  getP2PKPubkeyFromProofs,
+  getUnspentProofsFromToken,
+  isP2PKSecret,
+  isPlainSecret,
+} from '~/lib/cashu';
+import { checkIsTestMint, getMintInfo } from '~/lib/cashu';
+import type { AccountWithBadges } from '../accounts/account-selector';
+import {
+  useCreateCashuTokenSwap,
+  useTokenSwap,
+} from './cashu-token-swap-hooks';
+
+type CashuAccountWithBadges = AccountWithBadges<CashuAccount>;
+
+type UseReceiveCashuTokenProps = {
+  token: Token;
+  cashuPubKey: string;
+};
+
+type TokenQueryResult =
+  | {
+      /** The token with only claimable proofs. Will be null if the token cannot be claimed */
+      claimableToken: null;
+      /** The reason why the token cannot be claimed. Will always be defined when claimableToken is null */
+      cannotClaimReason: string;
+    }
+  | {
+      claimableToken: Token;
+      cannotClaimReason: null;
+    };
+
+/**
+ * Takes a token and returns the account that the token is from.
+ * If the account does not exist, we construct and return an account, but we do not store it in the database.
+ */
+export function useGetCashuTokenSourceAccount(token: Token) {
+  const tokenCurrency = tokenToMoney(token).currency;
+  const { data: allAccounts } = useAccounts();
+  const accounts: CashuAccount[] = allAccounts.filter(
+    (a) => a.type === 'cashu',
+  );
+  const existingAccount = accounts.find(
+    (a) => a.mintUrl === token.mint && a.currency === tokenCurrency,
+  );
+  if (existingAccount) {
+    return existingAccount;
+  }
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['token-source-account', token.mint, tokenCurrency],
+    queryFn: async (): Promise<CashuAccount> => {
+      const info = await getMintInfo(token.mint);
+      const isTestMint = await checkIsTestMint(token.mint);
+      return {
+        id: '',
+        type: 'cashu',
+        mintUrl: token.mint,
+        createdAt: new Date().toISOString(),
+        name: info?.name ?? 'Unknown Mint',
+        currency: tokenToMoney(token).currency,
+        isTestMint: isTestMint ?? false,
+        version: 0,
+        keysetCounters: {},
+        proofs: [],
+      };
+    },
+    staleTime: 3 * 60 * 1000,
+    retry: 1,
+  });
+
+  return data;
+}
+
+const getClaimableProofs = (unspentProofs: Proof[], cashuPubKey: string) => {
+  let cannotClaimReason: string | undefined;
+  const claimableProofs = unspentProofs.filter((proof) => {
+    if (isPlainSecret(proof.secret)) {
+      return true;
+    }
+    if (!isP2PKSecret(proof.secret)) {
+      cannotClaimReason = 'This ecash contains an unknown spending condition.';
+      return false;
+    }
+    try {
+      const pubkey = getP2PKPubkeyFromProofs([proof]);
+      return pubkey === null || pubkey === cashuPubKey;
+    } catch {
+      console.warn('Failed to get pubkey from proof', proof);
+      return false;
+    }
+  });
+  if (claimableProofs.length === 0) {
+    return {
+      claimableProofs: null,
+      cannotClaimReason:
+        cannotClaimReason ?? 'You do not have permission to claim this ecash',
+    };
+  }
+  return { claimableProofs, cannotClaimReason: null };
+};
+
+/**
+ * This hook is used to check which proofs in a token are already spent and which
+ * proofs have spending conditions that this user can satisfy.
+ * @param token - The token to receive
+ * @param cashuPubKey - The public key that the the user can provide signatures for
+ * @returns A token with only proofs that can be claimed. If the token cannot be claimed,
+ * the hook will return a reason why and a null token.
+ */
+export function useGetClaimableToken({
+  token,
+  cashuPubKey,
+}: UseReceiveCashuTokenProps) {
+  const { data: tokenData } = useSuspenseQuery({
+    queryKey: ['token-state', token],
+    queryFn: async (): Promise<TokenQueryResult> => {
+      const unspentProofs = await getUnspentProofsFromToken(token);
+      if (unspentProofs.length === 0) {
+        return {
+          claimableToken: null,
+          cannotClaimReason: 'This ecash has already been spent',
+        };
+      }
+
+      const { claimableProofs, cannotClaimReason } = getClaimableProofs(
+        unspentProofs,
+        cashuPubKey,
+      );
+
+      return claimableProofs
+        ? {
+            claimableToken: { ...token, proofs: claimableProofs },
+            cannotClaimReason: null,
+          }
+        : { cannotClaimReason, claimableToken: null };
+    },
+    retry: 1,
+  });
+
+  return tokenData;
+}
+
+function isSameAccount(a: CashuAccount, b: CashuAccount) {
+  return a.mintUrl === b.mintUrl && a.currency === b.currency;
+}
+
+const getDefaultReceiveAccount = (
+  selectableAccounts: CashuAccount[],
+  sourceAccount: CashuAccount,
+  isCrossMintSwapDisabled: boolean,
+  defaultAccount: Account,
+): CashuAccount => {
+  const targetAccount =
+    isCrossMintSwapDisabled || defaultAccount.type !== 'cashu'
+      ? sourceAccount
+      : defaultAccount;
+
+  const matchingAccount = selectableAccounts.find((account) =>
+    isSameAccount(account, targetAccount),
+  );
+
+  // Fall back to source account if no match found
+  return matchingAccount ?? sourceAccount;
+};
+
+const getBadges = (
+  account: CashuAccount,
+  allAccounts: CashuAccount[],
+  sourceAccount: CashuAccount,
+  defaultAccount: Account,
+) => {
+  const badges: string[] = [];
+
+  if (sourceAccount.isTestMint) {
+    badges.push('Test Mint');
+  }
+  if (account.mintUrl === sourceAccount.mintUrl) {
+    badges.push('Source');
+  }
+  if (
+    defaultAccount.type === 'cashu' &&
+    isSameAccount(account, defaultAccount)
+  ) {
+    badges.push('Default');
+  }
+  if (!allAccounts.some((a) => a.mintUrl === account.mintUrl)) {
+    badges.push('Unknown');
+  }
+
+  return badges;
+};
+
+/**
+ * Returns a list of accounts that the user can select to receive the token.
+ * If cross mint swaps are disabled because we can't make a lightning payment
+ * from the source account, we only show the source account. Otherwise, we show
+ * the source account and all other non-testnet cashu accounts.
+ */
+const getSelectableAccounts = (
+  sourceAccount: CashuAccount,
+  isCrossMintSwapDisabled: boolean,
+  accounts: CashuAccount[],
+) => {
+  return isCrossMintSwapDisabled
+    ? [sourceAccount]
+    : [
+        sourceAccount,
+        ...accounts.filter(
+          (account) =>
+            !account.isTestMint && account.mintUrl !== sourceAccount.mintUrl,
+        ),
+      ];
+};
+
+/**
+ * Lets the user select an account to receive the token and returns data about the
+ * selectable accounts based on the source account and the user's accounts in the database.
+ */
+export function useReceiveCashuTokenAccounts(sourceAccount: CashuAccount) {
+  const { data: allAccounts } = useAccounts();
+  const defaultAccount = useDefaultAccount();
+  const accounts: CashuAccount[] = allAccounts.filter(
+    (a) => a.type === 'cashu',
+  );
+  const isCrossMintSwapDisabled = sourceAccount.isTestMint;
+  const selectableAccounts = getSelectableAccounts(
+    sourceAccount,
+    isCrossMintSwapDisabled,
+    accounts,
+  );
+  const defaultReceiveAccount = getDefaultReceiveAccount(
+    selectableAccounts,
+    sourceAccount,
+    isCrossMintSwapDisabled,
+    defaultAccount,
+  );
+
+  const [receiveAccount, setReceiveAccountState] =
+    useState<CashuAccountWithBadges>(defaultReceiveAccount);
+
+  const accountWithBadges = selectableAccounts.map((a) => ({
+    ...a,
+    badges: getBadges(a, accounts, sourceAccount, defaultAccount),
+  }));
+
+  const isSourceAccountAdded = accounts.some((a) =>
+    isSameAccount(a, sourceAccount),
+  );
+
+  const setReceiveAccount = (account: CashuAccountWithBadges) => {
+    const isSelectable = selectableAccounts.some((a) =>
+      isSameAccount(a, account),
+    );
+    if (!isSelectable) {
+      throw new Error('Account is not selectable');
+    }
+
+    setReceiveAccountState(account);
+  };
+
+  return {
+    selectableAccounts: accountWithBadges,
+    receiveAccount,
+    receiveAccountIsSource: receiveAccount.mintUrl === sourceAccount.mintUrl,
+    isSourceAccountAdded,
+    isCrossMintSwapDisabled,
+    setReceiveAccount,
+  };
+}
+
+export function useReceiveCashuToken() {
+  const [status, setStatus] = useState<
+    'idle' | 'pending' | 'success' | 'error'
+  >('idle');
+  const { mutate: createSwap, data: swapData } = useCreateCashuTokenSwap();
+  useTokenSwap({
+    tokenHash: swapData?.tokenHash,
+    onCompleted: () => {
+      setStatus('success');
+    },
+  });
+
+  const { mutate: startReceive } = useMutation({
+    mutationFn: async ({
+      token,
+      account,
+    }: { token: Token; account: CashuAccount }) => {
+      const isSource = account.mintUrl === token.mint;
+      if (isSource) {
+        createSwap({
+          token,
+          account,
+        });
+      } else {
+        // TODO: implement cross mint swap and  claim to other account types
+        throw new Error('Not implemented');
+      }
+    },
+    onMutate: () => {
+      setStatus('pending');
+    },
+    onError: (error) => {
+      setStatus('error');
+      console.error('Failed to create cashu token swap', error);
+    },
+  });
+
+  return {
+    status,
+    startReceive,
+  };
+}
