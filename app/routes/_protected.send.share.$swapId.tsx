93c5bb238255b8dfe8ff00961f849530d62eef8d
gudnuf <108303703+gudnuf@users.noreply.github.com>
2025-05-26 20:48:14 -0500

    Ensure account exists for send share (#405)

--- app/routes/_protected.send.share.$swapId.tsx
+++ app/routes/_protected.send.share.$swapId.tsx
@@ -1,21 +1,21 @@
 import { Page } from '~/components/page';
 import { Redirect } from '~/components/redirect';
-import { useAccountsCache } from '~/features/accounts/account-hooks';
 import { LoadingScreen } from '~/features/loading/LoadingScreen';
 import { useCashuSendSwap } from '~/features/send/cashu-send-swap-hooks';
 import { ShareCashuToken } from '~/features/send/share-cashu-token';
 import { SuccessfulSendPage } from '~/features/send/succesful-send-page';
+import type { Account } from '~/features/accounts/account';
 import { getCashuProtocolUnit } from '~/lib/cashu';
 import type { Route } from './+types/_protected.send.share.$swapId';
 
 export default function SendShare({ params }: Route.ComponentProps) {
-  const accountsCache = useAccountsCache();
   const { swapId } = params;
 
   const {
     data: swap,
     status,
     error,
+    account,
   } = useCashuSendSwap({
     id: swapId,
   });
@@ -25,18 +25,19 @@ export default function SendShare({ params }: Route.ComponentProps) {
   }
 
   if (error) {
-    return <Redirect to="/send" logMessage="Error fetching swap" />;
+    return (
+      <Redirect
+        to="/404?message=Cashu%20send%20not%20fount"
+        logMessage="Error fetching swap"
+      />
+    );
   }
 
   if (swap.state === 'COMPLETED') {
-    const account = accountsCache.get(swap.accountId);
-    if (!account) {
-      return <Redirect to="/send" logMessage="Account not found" />;
-    }
     return (
       <SuccessfulSendPage
         amount={swap.amountToSend}
-        account={account}
+        account={account as Account}
         destination={'ecash'}
         feesPaid={swap.sendSwapFee.add(swap.receiveSwapFee)}
       />
