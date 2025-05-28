e0509003b36815981f7c9cf6bc5a124df7f32286
gudnuf <108303703+gudnuf@users.noreply.github.com>
2025-05-28 10:31:04 -0500

    make account page layout static when setting default (#408)

--- app/features/settings/accounts/$account_id.tsx
+++ app/features/settings/accounts/$account_id.tsx
@@ -46,14 +46,16 @@ function CashuAccount({ account }: { account: ExtendedCashuAccount }) {
 
   return (
     <>
-      <div className="flex w-full flex-col gap-20 pt-4">
-        <div className="flex flex-col gap-20">
+      <div className="flex w-full flex-col gap-12 pt-4">
+        <div className="flex flex-col gap-12">
           <h1 className="text-center text-2xl">{account.name}</h1>
           <MoneyWithConvertedAmount money={getAccountBalance(account)} />
         </div>
 
         <div className="w-full space-y-8 sm:max-w-sm">
-          {account.isDefault && <Badge>Default</Badge>}
+          <div className="h-[24px]">
+            {account.isDefault && <Badge>Default</Badge>}
+          </div>
           {[
             { label: 'Type', value: account.type },
             {
@@ -68,11 +70,11 @@ function CashuAccount({ account }: { account: ExtendedCashuAccount }) {
         </div>
       </div>
 
-      {!account.isDefault && (
-        <div className="fixed right-0 bottom-16 left-0 flex justify-center">
+      <div className="fixed right-0 bottom-16 left-0 flex h-[40px] justify-center">
+        {!account.isDefault && (
           <Button onClick={makeDefault}>Make default</Button>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 }
