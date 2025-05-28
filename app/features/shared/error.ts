f1c598f487d17f00fa22187fdc72680cf38221e4
gudnuf <gudnuf21@proton.me>
2025-05-27 17:36:09 -0700

    toast if username already taken

--- app/features/shared/error.ts
+++ app/features/shared/error.ts
@@ -1,3 +1,5 @@
+import type { PostgrestError } from '@supabase/supabase-js';
+
 export const getErrorMessage = (
   error: unknown,
   fallbackMessage = 'Unknown error. Please try again or contact support',
@@ -12,3 +14,15 @@ export const getErrorMessage = (
 
   return fallbackMessage;
 };
+
+export class UniqueConstraintError extends Error {
+  constructor(postgresError: PostgrestError, message?: string) {
+    if (postgresError.code !== '23505') {
+      throw new Error('Expected a unique constraint error', {
+        cause: postgresError,
+      });
+    }
+
+    super(message ?? postgresError.message, { cause: postgresError });
+  }
+}
