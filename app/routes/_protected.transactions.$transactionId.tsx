4cfe7aa6337fe249067da09ef5ec822832f8246e
gudnuf <gudnuf21@proton.me>
2025-06-03 13:48:08 -0700

    create useReverseTransaction and nits

--- app/routes/_protected.transactions.$transactionId.tsx
+++ app/routes/_protected.transactions.$transactionId.tsx
@@ -4,7 +4,6 @@ import {
   PageContent,
   PageHeader,
 } from '~/components/page';
-import { useUnresolvedCashuSendSwaps } from '~/features/send/cashu-send-swap-hooks';
 import { TransactionDetails } from '~/features/transactions/transaction-details';
 import { useSuspenseTransaction } from '~/features/transactions/transaction-hooks';
 import type { Route } from './+types/_protected.transactions.$transactionId';
@@ -13,12 +12,6 @@ export default function TransactionDetailsPage({
   params: { transactionId },
 }: Route.ComponentProps) {
   const { data: transaction } = useSuspenseTransaction(transactionId);
-
-  // QUESTION: Should we instead make a useGetCashuSendSwapByTransactionId hook?
-  const { pending } = useUnresolvedCashuSendSwaps();
-  const pendingCashuSendSwap = pending.find(
-    (swap) => swap.transactionId === transactionId,
-  );
 
   return (
     <Page>
@@ -30,10 +23,7 @@ export default function TransactionDetailsPage({
         />
       </PageHeader>
       <PageContent>
-        <TransactionDetails
-          transaction={transaction}
-          pendingCashuSendSwap={pendingCashuSendSwap}
-        />
+        <TransactionDetails transaction={transaction} />
       </PageContent>
     </Page>
   );
