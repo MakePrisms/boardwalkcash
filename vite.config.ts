75e085ee3a29f01afee405ef1893ea361f4ec7ef
Josip Bojčić <jbojcic1@gmail.com>
2025-06-12 22:43:23 +0200

    Add vite plugin for detools json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
   build: {
     emptyOutDir: false,
   },
