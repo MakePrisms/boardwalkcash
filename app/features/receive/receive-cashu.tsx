e5c3cca4a4365dffd17decb120ddf9a25386da76
gudnuf <gudnuf21@proton.me>
2025-06-19 19:33:12 -0700

    remove cashu payment request related code

--- app/features/receive/receive-cashu.tsx
+++ app/features/receive/receive-cashu.tsx
@@ -1,4 +1,3 @@
-import { Banknote, Zap } from 'lucide-react';
 import { useState } from 'react';
 import { useCopyToClipboard } from 'usehooks-ts';
 import {
@@ -11,54 +10,17 @@ import { QRCode } from '~/components/qr-code';
 import {
   Carousel,
   CarouselContent,
-  CarouselControls,
   CarouselItem,
 } from '~/components/ui/carousel';
 import type { CashuAccount } from '~/features/accounts/account';
 import { useToast } from '~/hooks/use-toast';
-import { getCashuProtocolUnit } from '~/lib/cashu';
 import type { Money } from '~/lib/money';
 import { MoneyWithConvertedAmount } from '../shared/money-with-converted-amount';
 import {
   useCashuReceiveQuote,
   useCreateCashuReceiveQuote,
 } from './cashu-receive-quote-hooks';
-import { getCashuRequest } from './reusable-payment-request';
 import { SuccessfulReceivePage } from './successful-receive-page';
-
-type CashuRequestItemProps = {
-  account: CashuAccount;
-  amount: Money;
-};
-
-function CashuRequestCarouselItem({ account, amount }: CashuRequestItemProps) {
-  const cashuUnit = getCashuProtocolUnit(account.currency);
-  const [, copyToClipboard] = useCopyToClipboard();
-  const { toast } = useToast();
-  // TODO: this should come from some hook that does a similar thing to the mint quote hook
-  const cashuRequest = getCashuRequest(account, {
-    amount,
-    unit: cashuUnit,
-    singleUse: true,
-  }).toEncodedRequest();
-
-  return (
-    <CarouselItem>
-      <QRCode
-        value={cashuRequest}
-        description="Scan with any wallet that supports Cashu payment requests."
-        onClick={() => {
-          copyToClipboard(cashuRequest);
-          toast({
-            title: 'Copied Cashu Payment Request',
-            description: `${cashuRequest?.slice(0, 5)}...${cashuRequest?.slice(-5)}`,
-            duration: 1000,
-          });
-        }}
-      />
-    </CarouselItem>
-  );
-}
 
 type MintQuoteProps = {
   account: CashuAccount;
@@ -147,17 +109,12 @@ export default function ReceiveCashu({ amount, account }: Props) {
 
         <Carousel opts={{ align: 'center', loop: true }}>
           <CarouselContent>
-            <CashuRequestCarouselItem account={account} amount={amount} />
             <MintQuoteCarouselItem
               account={account}
               amount={amount}
               onPaid={() => setStatus('received')}
             />
           </CarouselContent>
-          <CarouselControls>
-            <Banknote className="h-5 w-5" />
-            <Zap className="h-5 w-5" />
-          </CarouselControls>
         </Carousel>
       </PageContent>
     </>
