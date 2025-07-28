269a20ccc91df33d5baef2ac9ec24089cda7cd17
gudnuf <108303703+gudnuf@users.noreply.github.com>
2025-07-28 15:53:54 -0500

    refactor receive-cashu-token-hooks (#529)
    * refactor receive-cashu-token-hooks
    
    * feedback
    
    * nits
    
    * fail receive quote transaction too

--- app/hooks/use-exchange-rate.ts
+++ app/hooks/use-exchange-rate.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { type Ticker, exchangeRateService } from '~/lib/exchange-rate';
 
 export const useExchangeRate = (ticker: Ticker) => {
@@ -27,4 +27,26 @@ export const useExchangeRates = (tickers: Ticker[]) => {
     },
     refetchInterval: 15_000,
   });
+};
+
+/**
+ * Returns a function that can be used to get the exchange rate for a given ticker.
+ * The function will check the cache and if not found, it will fetch the rate.
+ */
+export const useGetExchangeRate = () => {
+  const queryClient = useQueryClient();
+
+  return async (ticker: Ticker): Promise<string> => {
+    return queryClient.fetchQuery({
+      queryKey: ['exchangeRate', ticker],
+      queryFn: async ({ signal }) => {
+        return exchangeRateService
+          .getRates({
+            tickers: [ticker],
+            signal,
+          })
+          .then((rates) => rates[ticker]);
+      },
+    });
+  };
 };
--- app/lib/cashu/index.ts
+++ app/lib/cashu/index.ts
@@ -1,7 +1,6 @@
 export * from './proof';
 export * from './secret';
 export * from './token';
-export * from './cross-mint-quotes';
 export * from './utils';
 export * from './error-codes';
 export type { MintInfo } from './types';
